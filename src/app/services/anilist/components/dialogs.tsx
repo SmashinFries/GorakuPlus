b356f763971192fe6892333bd3cbeac068f6d695
SmashinFries <smashinfries1@gmail.com>
2023-05-19 01:39:10 -0500

    convert login to logout dialog

--- src/app/services/anilist/components/dialogs.tsx
+++ src/app/services/anilist/components/dialogs.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Platform, View } from 'react-native';
-import { ActivityIndicator, Button, Dialog, Text } from 'react-native-paper';
+import { ActivityIndicator, Button, Dialog, MD3Colors, Text } from 'react-native-paper';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../../store';
 import { useAnilistAuth } from '../hooks/authAni';
@@ -9,50 +9,42 @@ import { RootNavPaths } from '../../../../navigation/types';
 type AniListLoginDialogProps = {
     visible: boolean;
     onDismiss: () => void;
-    rootPath: keyof RootNavPaths;
     onLogout: () => void;
+    onRelogin: () => void;
 };
-const AniListLoginDialog = ({
+const AniListLogoutDialog = ({
     visible,
     onDismiss,
-    rootPath,
     onLogout,
+    onRelogin,
 }: AniListLoginDialogProps) => {
-    const { timeTillDeath, token } = useSelector((state: RootState) => state.persistedAniLogin);
-    const { request, promptAsync, result } = useAnilistAuth(rootPath);
     const isWeb = Platform.OS === 'web';
 
-    const LoginInfo = (
-        <Dialog.Content>
-            <Text variant="titleLarge">Time till next login:</Text>
-            <Text variant="bodyLarge">{timeTillDeath}</Text>
-        </Dialog.Content>
-    );
-
-    const LoginButton = () => (
+    const LogoutButton = () => (
         <Button
-            onPress={() => {
-                token ? onLogout() : promptAsync().catch((e) => console.log(e));
-            }}
+            onPress={() => {onLogout(); onDismiss();}}
+            textColor={MD3Colors.error50}
         >
-            {token ? 'Logout' : 'Login'}
+            {'Logout'}
         </Button>
     );
+
+    const RefetchButton = () => <Button onPress={() => {onRelogin(); onDismiss();}}>Re-login</Button>
 
     return (
         <Dialog
             visible={visible}
             onDismiss={onDismiss}
-            style={{ maxWidth: isWeb ? '50%' : null, alignSelf: isWeb ? 'center' : null }}
+            style={{ maxWidth: isWeb ? '50%' : undefined, alignSelf: isWeb ? 'center' : undefined }}
         >
-            <Dialog.Title>{!token ? 'Login to Anilist?' : 'Logout of Anilist?'}</Dialog.Title>
-            {token ? LoginInfo : null}
+            <Dialog.Title>{'Logout of Anilist?'}</Dialog.Title>
             <Dialog.Actions>
                 <Button onPress={onDismiss}>Cancel</Button>
-                {<LoginButton />}
+                <RefetchButton />
+                <LogoutButton />
             </Dialog.Actions>
         </Dialog>
     );
 };
 
-export default AniListLoginDialog;
+export default AniListLogoutDialog;
