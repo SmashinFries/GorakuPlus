6f4946c5cf2c2a7bd6d2f8048b2ac371509c2a0b
SmashinFries <smashinfries1@gmail.com>
2023-06-11 22:56:38 -0500

    add user data to auth state

--- src/app/services/anilist/authSlice.ts
+++ src/app/services/anilist/authSlice.ts
@@ -5,20 +5,30 @@ import type { RootState } from '../../store';
 export type AuthState = {
     token: string | null;
     deathDate: string | null;
+    username?: string | null;
+    avatar?: string | null;
+    userID?: number | null;
 };
 
 const slice = createSlice({
     name: 'authAnilist',
-    initialState: { token: null, deathDate: null } as AuthState,
+    initialState: {
+        token: null,
+        deathDate: null,
+        avatar: null,
+        userID: null,
+        username: null,
+    } as AuthState,
     reducers: {
         setAniAuth: (
             state,
-            {
-                payload: { token, deathDate },
-            }: PayloadAction<{ token: string; deathDate: string }>,
+            { payload: { token, deathDate, avatar, userID, username } }: PayloadAction<AuthState>,
         ) => {
             state.token = token;
             state.deathDate = deathDate;
+            state.avatar = avatar;
+            state.userID = userID;
+            state.username = username;
         },
     },
 });
