4dedba0e6d1a8c746386b3a8e703deaf4be6f943
SmashinFries <smashinfries1@gmail.com>
2023-06-11 22:56:12 -0500

    add slices and settings storage

--- src/app/store.ts
+++ src/app/store.ts
@@ -18,6 +18,10 @@ import themeSlice, { ThemeState } from '../theme/themeSlice';
 import createSecureStorage from './persist-securestore';
 import { Platform } from 'react-native';
 import authSlice, { AuthState } from './services/anilist/authSlice';
+import danbooruApi from './services/danbooru/danbooruApi';
+import { api as anilistApi } from './services/anilist/enhanced';
+import settingsSlice, { SettingsState } from '../features/more/settings/settingsSlice';
+import { malApi } from './services/mal/malApi';
 
 const secureStorage = createSecureStorage();
 
@@ -32,12 +36,16 @@ const anilistAuthPersistConfig: PersistConfig<any, any, any, any> = {
 };
 
 const persistedTheme = persistReducer<ThemeState, AnyAction>(persistConfig, themeSlice);
+const persistedSettings = persistReducer<SettingsState, AnyAction>(persistConfig, settingsSlice);
 const persistedAniLogin = persistReducer<AuthState, AnyAction>(anilistAuthPersistConfig, authSlice);
 
 export const store = configureStore({
     reducer: {
-        // [anilistApi.reducerPath]: anilistApi.reducer,
+        [anilistApi.reducerPath]: anilistApi.reducer,
+        [danbooruApi.reducerPath]: danbooruApi.reducer,
+        [malApi.reducerPath]: malApi.reducer,
         persistedTheme,
+        persistedSettings,
         persistedAniLogin,
     },
     middleware: (getDefaultMiddleware) =>
@@ -45,7 +53,7 @@ export const store = configureStore({
             serializableCheck: {
                 ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
             },
-        })
+        }).concat([anilistApi.middleware, malApi.middleware, danbooruApi.middleware]),
 });
 
 export const persistor = persistStore(store);
