3431d9e11bfba1adde8e18e9d1d615e668eaaa1f
SmashinFries <smashinfries1@gmail.com>
2023-06-11 23:00:27 -0500

    add error component

---
+++ src/components/error.tsx
@@ -0,0 +1,27 @@
+import { QueryStatus } from '@reduxjs/toolkit/dist/query';
+import { MotiView } from 'moti';
+import { StyleSheet } from 'react-native';
+import { IconButton, Text, useTheme } from 'react-native-paper';
+
+export const NetworkError = ({ status }: { status: QueryStatus }) => {
+    const { colors } = useTheme();
+
+    return (
+        <MotiView style={[styles.container]}>
+            <MotiView>
+                <IconButton icon="server-network-off" size={38} iconColor={colors.error} />
+            </MotiView>
+            <Text>
+                {status === QueryStatus.rejected ? 'Network Error!' : 'Something went wrong!'}
+            </Text>
+        </MotiView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
