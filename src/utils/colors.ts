bc35aea64a9d7e91e0c53cc1ce964635d9b32dd6
SmashinFries <smashinfries1@gmail.com>
2023-06-19 20:24:06 -0500

    add color functions

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,18 +1,52 @@
 import { MediaListStatus } from '../app/services/anilist/generated-anilist';
 
+const RED = '#FF0000';
+const YELLOW = '#FFA500';
+const GREEN = '#00d400';
+
 const between = (score: number, min: number, max: number): boolean => {
     return score >= min && score <= max;
 };
 
-export const getScoreColor = (score: number): string => {
-    if (between(score, 0, 64)) {
-        return '#FF0000';
-    } else if (between(score, 65, 74)) {
-        return '#FFA500';
-    } else if (between(score, 75, 100)) {
-        return '#00d400';
-    } else {
-        return '#000000';
+const getMalColor = (
+    score: number,
+    colors: {
+        red: number;
+        yellow: number;
+    },
+) => {
+    const maxRed = colors.red * 0.1;
+    const maxYellow = colors.yellow * 0.1;
+    if (between(score, 0, maxRed)) {
+        return RED;
+    } else if (between(score, maxRed, maxYellow)) {
+        return YELLOW;
+    } else if (between(score, maxYellow, 10)) {
+        return GREEN;
+    }
+};
+
+export const getScoreColor = (
+    score: number,
+    colors: {
+        red: number;
+        yellow: number;
+    },
+    isMal = false,
+): string => {
+    const maxRed = colors.red;
+    const maxYellow = colors.yellow;
+
+    if (isMal) {
+        return getMalColor(score, colors);
+    }
+
+    if (between(score, 0, colors.red)) {
+        return RED;
+    } else if (between(score, maxRed + 1, maxYellow)) {
+        return YELLOW;
+    } else if (between(score, maxYellow + 1, 100)) {
+        return GREEN;
     }
 };
 
