a477e105c4d5313c55508e751a151deb5a4e6d1d
SmashinFries <smashinfries1@gmail.com>
2023-06-19 20:24:28 -0500

    ignore firebase notifications

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,14 +1,14 @@
-import messaging from '@react-native-firebase/messaging';
+// import messaging from '@react-native-firebase/messaging';
 
-// FB Cloud Messaging
-export const requestUserNotifPermission = async () => {
-    // Android Does not need this but may be useful for future iOS support!
-    const authStatus = await messaging().requestPermission();
-    const enabled =
-        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+// // FB Cloud Messaging
+// export const requestUserNotifPermission = async () => {
+//     // Android Does not need this but may be useful for future iOS support!
+//     const authStatus = await messaging().requestPermission();
+//     const enabled =
+//         authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+//         authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-    if (enabled) {
-        console.log('Authorization status:', authStatus);
-    }
-};
+//     if (enabled) {
+//         console.log('Authorization status:', authStatus);
+//     }
+// };
