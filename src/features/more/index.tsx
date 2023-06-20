186b1cc3171072643b7f942d94c0c2c4ee73e25c
SmashinFries <smashinfries1@gmail.com>
2023-06-19 20:12:48 -0500

    add about section

--- src/features/more/index.tsx
+++ src/features/more/index.tsx
@@ -1,30 +1,33 @@
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { View } from "react-native";
-import { Divider, List } from "react-native-paper";
-import { MoreStackProps } from "../../navigation/types";
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { View } from 'react-native';
+import { Divider, List, useTheme } from 'react-native-paper';
+import { MoreStackProps } from '../../navigation/types';
 
-const MoreScreen = ({navigation}:NativeStackScreenProps<MoreStackProps, 'more'>) => {
-    return(
+const MoreScreen = ({ navigation }: NativeStackScreenProps<MoreStackProps, 'more'>) => {
+    const { colors } = useTheme();
+    return (
         <View>
             <Divider />
             <List.Item
                 title="Accounts"
                 onPress={() => navigation.navigate('accounts')}
                 // description="Manage accounts"
-                left={props => <List.Icon {...props} icon="account-outline" />}
-            />
-            <List.Item
-                title="Statistics"
-                // description="Customize your UI"
-                left={props => <List.Icon {...props} icon="chart-line" />}
+                left={(props) => (
+                    <List.Icon {...props} color={colors.primary} icon="account-outline" />
+                )}
             />
             <List.Item
                 title="Settings"
                 // description="Customize your UI"
                 onPress={() => navigation.navigate('settings')}
-                left={props => <List.Icon {...props} icon="cog-outline" />}
+                left={(props) => <List.Icon {...props} color={colors.primary} icon="cog-outline" />}
             />
-            
+            <List.Item
+                title="About"
+                left={(props) => (
+                    <List.Icon {...props} color={colors.primary} icon="information-outline" />
+                )}
+            />
         </View>
     );
 };
