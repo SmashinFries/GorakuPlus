b644f91f7f46a46b44df00ba87133a8e930cebb7
SmashinFries <smashinfries1@gmail.com>
2023-06-19 20:04:09 -0500

    add list subheader component

---
+++ src/components/titles.tsx
@@ -0,0 +1,9 @@
+import { List, useTheme } from 'react-native-paper';
+
+type ListSubheaderProps = {
+    title: string;
+};
+export const ListSubheader = ({ title }: ListSubheaderProps) => {
+    const { colors } = useTheme();
+    return <List.Subheader style={{ color: colors.primary }}>{title}</List.Subheader>;
+};
