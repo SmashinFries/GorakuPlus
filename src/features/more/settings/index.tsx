39a4c51f6bb454790638928f142e6c712cef0325
SmashinFries <smashinfries1@gmail.com>
2023-06-19 20:13:28 -0500

    add options

--- src/features/more/settings/index.tsx
+++ src/features/more/settings/index.tsx
@@ -1,53 +1,71 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ThemeOptions } from '../../../theme/theme';
 import { useDispatch, useSelector } from 'react-redux';
 import { setTheme } from '../../../theme/themeSlice';
-import { List, RadioButton, Switch } from 'react-native-paper';
+import { List, RadioButton, Switch, useTheme } from 'react-native-paper';
 import { RootState } from '../../../app/store';
-import { ScrollView, View } from 'react-native';
+import { ScrollView } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
 import { api } from '../../../app/services/anilist/enhanced';
-import { MoreStackProps, RootNavPaths } from '../../../navigation/types';
+import { MoreStackProps, SettingsStackProps } from '../../../navigation/types';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 WebBrowser.maybeCompleteAuthSession();
 
-const SettingsScreen = ({ navigation }: NativeStackScreenProps<MoreStackProps, 'settings'>) => {
-    const THEME_SELECTION: ThemeOptions[] = ['default', 'kawaii', 'punpun', 'sonozaki_mion'];
-    const { mode, isDark } = useSelector((state: RootState) => state.persistedTheme);
+const SettingsScreen = ({
+    navigation,
+}: NativeStackScreenProps<SettingsStackProps, 'settingsHome'>) => {
     const dispatch = useDispatch();
+    const { colors } = useTheme();
 
     const resetCache = () =>
         dispatch(api.util.invalidateTags(['ExploreAnime', 'ExploreManga', 'ExploreNovel']));
 
-    const onDarkChange = () => dispatch(setTheme({ isDark: !isDark, mode:mode }));
-
     return (
         <ScrollView>
-            <List.Section>
-                <List.Subheader>Appearance</List.Subheader>
-                <List.Item
-                    title={'Dark Mode'}
-                    onPress={() => {
-                        dispatch(setTheme({ isDark: !isDark, mode:mode }));
-                    }}
-                    // @ts-ignore
-                    right={(props) => <Switch value={isDark} {...props} onValueChange={onDarkChange} />}
-                />
-                {THEME_SELECTION.map((theme, index) => (
-                    <RadioButton.Item
-                        key={index}
-                        label={theme.replace('_', ' ')}
-                        status={mode === theme ? 'checked' : 'unchecked'}
-                        onPress={() => dispatch(setTheme({ mode: theme, isDark: isDark }))}
-                        value={theme}
-                        labelStyle={{ textTransform: 'capitalize' }}
-                    />
-                ))}
-            </List.Section>
+            <List.Item
+                title={'Appearance'}
+                description={'Themes, animations'}
+                onPress={() => {
+                    navigation.navigate('appearance');
+                }}
+                left={(props) => (
+                    <List.Icon {...props} color={colors.primary} icon="palette-outline" />
+                )}
+            />
+            <List.Item
+                title={'Tabs'}
+                description={'Order and visibility of tabs'}
+                onPress={() => {
+                    navigation.navigate('tabs');
+                }}
+                left={(props) => <List.Icon {...props} color={colors.primary} icon="tab" />}
+            />
+            <List.Item
+                title={'Media'}
+                description={'Change media defaults'}
+                onPress={() => {
+                    navigation.navigate('media');
+                }}
+                left={(props) => <List.Icon {...props} color={colors.primary} icon="television" />}
+            />
+            <List.Item
+                title={'Language'}
+                description={'Change app and media title langauge'}
+                onPress={() => {
+                    navigation.navigate('language');
+                }}
+                left={(props) => <List.Icon {...props} color={colors.primary} icon="translate" />}
+            />
+            {/* <List.Item
+                title={'Animations'}
+                description={'Adjust navigation animations'}
+                onPress={() => {
+                    navigation.navigate('appearance');
+                }}
+                left={(props) => <List.Icon {...props} icon="animation-outline" />}
+            /> */}
         </ScrollView>
-
-        // </View>
     );
 };
 
