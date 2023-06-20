26217555b5dd2a75d2ac7ed9be21da57c9331cc7
SmashinFries <smashinfries1@gmail.com>
2023-06-19 20:20:02 -0500

    add slideshow hook

---
+++ src/hooks/slideshow.ts
@@ -0,0 +1,42 @@
+import React, { useCallback, useReducer, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+
+type SlideShowProps = {
+    images: string[];
+};
+export const useSlideshow = ({ images }: SlideShowProps) => {
+    const [bg, setBg] = useState({ id: 0, bg: images[0] });
+    const [visible, toggle] = useReducer((s) => !s, true);
+
+    const imagesLength = images.length;
+
+    const updateBG = () => {
+        if (imagesLength > 1) {
+            const nextID = imagesLength - 1 === bg.id ? 0 : bg.id + 1;
+            const nextBG = images[nextID];
+            // console.log('Changed!', nextID, `LENGTH: ${imagesLength}`);
+            setBg({ id: nextID, bg: nextBG });
+            toggle();
+        }
+    };
+
+    useFocusEffect(
+        useCallback(() => {
+            if (imagesLength > 1) {
+                const interval = setInterval(() => {
+                    toggle();
+                }, 15000);
+                return () => {
+                    // console.log('clearing interval');
+                    clearInterval(interval);
+                };
+            }
+        }, [bg]),
+    );
+
+    if (imagesLength === 0) {
+        return { bg: '', visible, updateBG };
+    }
+
+    return { bg: bg.bg, visible, updateBG };
+};
