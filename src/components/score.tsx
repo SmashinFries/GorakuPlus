7b23c7f53c10e1d0a0af715960f94af2d796f080
SmashinFries <smashinfries1@gmail.com>
2023-06-19 20:03:34 -0500

    add score container

---
+++ src/components/score.tsx
@@ -0,0 +1,67 @@
+import { View } from 'react-native';
+import { Text, useTheme } from 'react-native-paper';
+import { TransYUpView } from './animations';
+import { MotiView } from 'moti';
+
+type ScoreContainerProps = {
+    title?: string;
+    score: number;
+    opacity?: number;
+    color?: string;
+    isMal?: boolean;
+    animate?: boolean;
+};
+export const ScoreContainer = ({
+    title,
+    color,
+    opacity,
+    score,
+    animate = true,
+    isMal = false,
+}: ScoreContainerProps) => {
+    const height = 85;
+    const fillHeight = score ? ((isMal ? score * 10 : score) / 100) * height : 0;
+    const width = 85;
+    const { colors } = useTheme();
+    return (
+        <TransYUpView animation={animate}>
+            <View
+                style={{
+                    height: height,
+                    width: width,
+                    borderRadius: height / 2,
+                    opacity: opacity,
+                    justifyContent: 'flex-end',
+                    alignItems: 'center',
+                    overflow: 'hidden',
+                }}
+            >
+                <MotiView
+                    from={{ height: 0 }}
+                    animate={{ height: fillHeight }}
+                    style={{
+                        position: 'absolute',
+                        // height: fillHeight,
+                        width: width,
+                        backgroundColor: color,
+                    }}
+                />
+            </View>
+            <View
+                style={{
+                    position: 'absolute',
+                    height: height,
+                    width: width,
+                    borderRadius: height / 2,
+                    borderWidth: 1,
+                    borderColor: score ? color : colors.outline,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <Text>{score ?? '?'}</Text>
+                {title ? <Text>{title}</Text> : null}
+            </View>
+        </TransYUpView>
+    );
+};
