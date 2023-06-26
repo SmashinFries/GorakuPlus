fc2196ec336b3b5e1aeb32426af48ce5b6e97118
SmashinFries <smashinfries1@gmail.com>
2023-06-25 19:45:24 -0500

    add filter logic

--- src/features/search/hooks/filter.ts
+++ src/features/search/hooks/filter.ts
@@ -1,53 +1,97 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import {
     ExploreMediaQueryVariables,
     MediaFormat,
-    MediaSort,
-    MediaType,
 } from '../../../app/services/anilist/generated-anilist';
-import { FilterOptions } from '../types';
-import { ANIME_DANGER, MANGA_DANGER } from '../constants/mediaConsts';
+import { FilterTypes } from '../types';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../../app/store';
+import {
+    FilterState,
+    addFilterParam,
+    changeSort,
+    removeFilterParam,
+    setMediaType,
+} from '../filterSlice';
+import { ANIME_FORMATS, MANGA_FORMATS, animeSort, mangaNovelSort } from '../constants/mediaConsts';
 
-export const useFilter = () => {
-    const [filterOptions, setFilterOptions] = useState<ExploreMediaQueryVariables>({
-        type: MediaType.Anime,
-        sort: MediaSort.TrendingDesc,
-    });
+export type FilterValues = {
+    filter: RootState['persistedFilter']['filter'];
+    sort: RootState['persistedFilter']['sort'];
+    current: RootState['persistedFilter']['current'];
+    updateFilter: (param: keyof ExploreMediaQueryVariables, value: any) => void;
+    removeFilter: (filter_param: keyof ExploreMediaQueryVariables) => void;
+    switchType: (m_type: FilterTypes) => void;
+    updateSort: (
+        mode: FilterState['sort']['mode'],
+        category: FilterState['sort']['category'],
+    ) => void;
+};
 
-    const updateFilter = <Opt extends keyof FilterOptions>(
-        option_type: Opt,
-        value: FilterOptions[Opt] | string,
+export const useFilter = (): FilterValues => {
+    const { current, filter, sort } = useSelector((state: RootState) => state.persistedFilter);
+    const { search } = useSelector((state: RootState) => state.persistedSearch);
+    const dispatch = useDispatch();
+
+    const updateFilter = (param: keyof ExploreMediaQueryVariables, value: any) => {
+        dispatch(addFilterParam({ param: param, value: value }));
+    };
+
+    const removeFilter = (filter_param: keyof ExploreMediaQueryVariables) => {
+        dispatch(removeFilterParam(filter_param));
+    };
+
+    const switchType = (m_type: FilterTypes) => {
+        dispatch(setMediaType(m_type));
+
+        // sort adjust
+        if (m_type === 'anime' && animeSort.includes(sort.category) === false) {
+            updateSort(sort.mode, 'Trending');
+        } else if (m_type !== 'anime' && mangaNovelSort.includes(sort.category) === false) {
+            updateSort(sort.mode, 'Trending');
+        }
+
+        // format adjust
+        if (m_type === 'anime' && ANIME_FORMATS.includes(filter.format) === false) {
+            removeFilter('format');
+            console.log('removed!');
+        } else if (
+            (m_type === 'manga' || m_type === 'manhwa') &&
+            MANGA_FORMATS.includes(filter.format) === false
+        ) {
+            removeFilter('format');
+        } else if (m_type === 'novel') {
+            updateFilter('format', MediaFormat.Novel);
+        }
+
+        // Country adjust
+        if (m_type === 'manga') {
+            updateFilter('countryOfOrigin', 'JP');
+        } else if (m_type === 'manhwa') {
+            updateFilter('countryOfOrigin', 'KR');
+        } else {
+            removeFilter('countryOfOrigin');
+        }
+    };
+
+    const updateSort = (
+        mode: FilterState['sort']['mode'],
+        category: FilterState['sort']['category'],
     ) => {
-        setFilterOptions((prev) => {
-            return { ...prev, [option_type]: value };
-        });
-    };
-
-    const switchType = (type: ExploreMediaQueryVariables['type'] | 'NOVEL') => {
-        if (type === 'NOVEL') {
-            updateFilter('type', MediaType.Manga);
-            updateFilter('format_in', [MediaFormat.Novel]);
-        } else {
-            updateFilter('type', type);
-            updateFilter('format_in', undefined);
-        }
+        dispatch(changeSort({ mode: mode, sort: category }));
     };
 
     useEffect(() => {
-        if (filterOptions.type === MediaType.Anime) {
-            setFilterOptions((prev) => {
-                return { ...prev, ...MANGA_DANGER };
-            });
-        } else {
-            setFilterOptions((prev) => {
-                return { ...prev, ...ANIME_DANGER };
-            });
-        }
-    }, [filterOptions.type]);
+        updateFilter('search', search);
+    }, [search]);
 
-    useEffect(() => {
-        console.log('filterOptions', filterOptions);
-    }, [filterOptions]);
-
-    return { filterOptions, updateFilter, switchType };
+    return {
+        filter,
+        sort,
+        current,
+        updateFilter,
+        removeFilter,
+        switchType,
+        updateSort,
+    };
 };
