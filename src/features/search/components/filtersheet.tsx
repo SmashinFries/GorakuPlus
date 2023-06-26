bd128cdef2d243bab48b05303e32fcf8108389df
SmashinFries <smashinfries1@gmail.com>
2023-06-25 19:44:37 -0500

    refactor

--- src/features/search/components/filtersheet.tsx
+++ src/features/search/components/filtersheet.tsx
@@ -1,8 +1,8 @@
-import React, { useCallback, useEffect, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { BottomSheetScrollView, BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet';
 import { BottomSheetModalMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
 import { View, StyleSheet } from 'react-native';
-import { List, Text, useTheme } from 'react-native-paper';
+import { Button, List, Portal, Text, useTheme } from 'react-native-paper';
 import {
     FormatDropdown,
     SeasonDropdown,
@@ -10,24 +10,21 @@ import {
     StatusDropdown,
     YearDropdown,
 } from './dropdown';
-import { UpdateFilter } from '../types/types';
-import { MANGA_DANGER } from '../constants/mediaConsts';
+import { PresetDialog } from './dialogs';
+import { FilterValues } from '../hooks/filter';
 import { NSFWSelector, OnListSelector } from './buttons';
 
 type FilterSheetProps = {
     sheetRef: React.Ref<BottomSheetModalMethods>;
-    filterOptions: any;
-    updateFilter: UpdateFilter;
+    filterData: FilterValues;
     handleSheetChange: (index: number) => void;
 };
-export const FilterSheet = ({
-    sheetRef,
-    filterOptions,
-    updateFilter,
-    handleSheetChange,
-}: FilterSheetProps) => {
+export const FilterSheet = ({ sheetRef, filterData, handleSheetChange }: FilterSheetProps) => {
+    const [presetDialogVisible, setPresetDialogVisible] = useState(false);
     const { colors } = useTheme();
     const snapPoints = useMemo(() => ['70%', '100%'], []);
+
+    const hidePresetDialog = () => setPresetDialogVisible(false);
 
     return (
         <BottomSheetModal
@@ -39,46 +36,61 @@ export const FilterSheet = ({
             <BottomSheetScrollView
                 contentContainerStyle={{ backgroundColor: colors.secondaryContainer }}
             >
+                {/* <PresetButton onPress={() => setPresetDialogVisible(true)} /> */}
                 <BottomSheetView style={[styles.dropdownRow]}>
-                    <OnListSelector filterOptions={filterOptions} updateFilter={updateFilter} />
-                    <NSFWSelector filterOptions={filterOptions} updateFilter={updateFilter} />
+                    <OnListSelector
+                        onList={filterData.filter.onList}
+                        updateOnList={(onList) => filterData.updateFilter('onList', onList)}
+                    />
+                    {/* <NSFWSelector filterOptions={filterOptions} updateFilter={updateFilter} /> */}
                 </BottomSheetView>
                 <BottomSheetView style={[styles.dropdownRow]}>
                     <SortDropdown
-                        mediaType={filterOptions.type}
-                        sortValue={filterOptions.sort}
-                        updateFilter={updateFilter}
+                        current={filterData.current}
+                        updateSort={(mode, category) => filterData.updateSort(mode, category)}
+                        sortData={filterData.sort}
                     />
                     <StatusDropdown
-                        statusValue={filterOptions.status}
-                        updateFilter={updateFilter}
+                        current={filterData.current}
+                        statusValue={filterData.filter.status}
+                        updateStatus={(status) => filterData.updateFilter('status', status)}
                     />
                 </BottomSheetView>
+                {/* <Button onPress={() => console.log(filterData.filter.sort)}>Print Sort</Button> */}
+                <Button onPress={() => console.log(filterData.filter.onList ?? 'undefined')}>
+                    Print onList
+                </Button>
+
                 <BottomSheetView style={[styles.dropdownRow]}>
                     <FormatDropdown
-                        mediaType={filterOptions.type}
-                        formatValue={filterOptions.format}
-                        updateFilter={updateFilter}
+                        current={filterData.current}
+                        formatValue={filterData.filter.format}
+                        updateFormat={(format) => filterData.updateFilter('format', format)}
                     />
                 </BottomSheetView>
-                {filterOptions.type === 'ANIME' && (
+                {filterData.current === 'anime' && (
                     <BottomSheetView>
                         <Text variant="titleLarge" style={{ paddingLeft: 10, paddingTop: 20 }}>
                             Season
                         </Text>
                         <BottomSheetView style={[styles.dropdownRow, { paddingTop: 8 }]}>
                             <SeasonDropdown
-                                seasonValue={filterOptions.season}
-                                updateFilter={updateFilter}
+                                seasonValue={filterData.filter.season}
+                                updateSeason={(season) => filterData.updateFilter('season', season)}
                             />
                             <YearDropdown
-                                yearValue={filterOptions.seasonYear}
-                                updateFilter={updateFilter}
+                                yearValue={filterData.filter.seasonYear}
+                                updateSeasonYear={(year) =>
+                                    filterData.updateFilter('seasonYear', year)
+                                }
                             />
                         </BottomSheetView>
                     </BottomSheetView>
                 )}
             </BottomSheetScrollView>
+            <Portal>
+                <PresetDialog visible={presetDialogVisible} hideDialog={hidePresetDialog} />
+            </Portal>
         </BottomSheetModal>
     );
 };
