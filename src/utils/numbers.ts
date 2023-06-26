7fe3413c663878bb28c855f9838b77ae66df8fc5
SmashinFries <smashinfries1@gmail.com>
2023-06-25 19:40:53 -0500

    refactor

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,6 +1,7 @@
-export const arrayRange = (start: number, stop: number, step: number, isString = false) => {
+export const arrayRange = (start: number, stop: number, step: number): string[] => {
     const newArray = Array.from({ length: (stop - start) / step + 1 }, (value, index) => {
-        return isString ? (start + index * step).toString() : start + index * step;
+        return (start + index * step).toString();
+        // return start + index * step;
     });
     return newArray;
 };
