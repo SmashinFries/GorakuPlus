a910585a724b522590604ffcae0a16176d85233c
Smashinfries <jus10luka@gmail.com>
2023-09-17 18:32:01 -0500

    add danbooru tag items

---
+++ src/features/art/components/danTag.tsx
@@ -0,0 +1,58 @@
+import { ScrollView, View } from 'react-native';
+import { Button, Chip, Portal } from 'react-native-paper';
+import { ListHeading } from '../../../components/text';
+import { copyToClipboard } from '../../../utils';
+import { useState } from 'react';
+import { DanTagDescDialog } from './tagDescription';
+
+type DanbooruTag = {
+    tag: string;
+    color: string;
+    onPress?: (tag: string) => void;
+};
+
+export const TagItem = ({ tag, color, onPress }: DanbooruTag) => {
+    return (
+        <Chip
+            compact
+            onPress={() => onPress(tag)}
+            onLongPress={() => copyToClipboard(tag)}
+            style={{ margin: 8, backgroundColor: color }}
+        >
+            {tag}
+        </Chip>
+    );
+};
+
+type TagSectionProps = {
+    tags: string;
+    title: string;
+    color: string;
+    disableWiki?: boolean;
+};
+
+export const TagSection = ({ title, tags, color, disableWiki = false }: TagSectionProps) => {
+    const [selectedTag, setSelectedTag] = useState<string>('');
+
+    if (!tags) {
+        return null;
+    }
+    return (
+        <View>
+            <ListHeading title={title} />
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                {tags.split(' ').map((tag, index) => (
+                    <TagItem
+                        color={color}
+                        tag={tag}
+                        key={index}
+                        onPress={(newTag) => (disableWiki ? null : setSelectedTag(newTag))}
+                    />
+                ))}
+            </ScrollView>
+            <Portal>
+                <DanTagDescDialog tag={selectedTag} onDimiss={() => setSelectedTag('')} />
+            </Portal>
+        </View>
+    );
+};
