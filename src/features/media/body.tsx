67bc539d4bb11573359ca1f7dfeb97975acbc751
Smashinfries <jus10luka@gmail.com>
2023-09-17 18:41:51 -0500

    replace moti component

--- src/features/media/body.tsx
+++ src/features/media/body.tsx
@@ -1,7 +1,7 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { MotiView } from 'moti';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { useTheme } from 'react-native-paper';
 
 type Props = {
@@ -10,19 +10,16 @@ type Props = {
 const BodyContainer = ({ children }: Props) => {
     const { colors } = useTheme();
     return (
-        <LinearGradient locations={[0.1, 0.25]} colors={['transparent', colors.background]}>
-            <MotiView style={[styles.container, { backgroundColor: 'transparent' }]}>
-                {children}
-            </MotiView>
+        <LinearGradient locations={[0.1, 0.2]} colors={['transparent', colors.background]}>
+            <View style={[styles.container, { backgroundColor: 'transparent' }]}>{children}</View>
         </LinearGradient>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        height: '100%',
-        width: '100%',
-        flex: 1,
+        // flex: 1,
+        overflow: 'visible',
         paddingTop: 100,
     },
 });
