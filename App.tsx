f1331294dbeb409ed2ffe31251d7474c55dc30ee
Smashinfries <jus10luka@gmail.com>
2023-09-17 18:13:12 -0500

    add notifications

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
+import 'expo-dev-client';
 import 'react-native-reanimated';
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
 import { AppProvider } from './src/provider';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import RootStackNavigation from './src/navigation/root';
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager';
+import * as Linking from 'expo-linking';
+import notifee, { EventType } from '@notifee/react-native';
+import { store } from './src/app/store';
+import { useAppSelector } from './src/app/hooks';
+import {
+    displayNotification,
+    parseNotif,
+} from './src/features/more/settings/notifications/backgroundFetch';
+import { useLinkTo } from '@react-navigation/native';
+import { useEffect } from 'react';
+import useNotif from './src/provider/hooks/useNotificationSetup';
+import { api } from './src/app/services/anilist/enhanced';
+
 // import * as Localization from 'expo-localization';
 // import { I18n } from 'i18n-js';
 // import en from './languages/english.json';
@@ -21,8 +35,67 @@ if (typeof window !== 'undefined') {
 // };
 // const i18n = new I18n(translations);
 
+notifee.onBackgroundEvent(async ({ type, detail }) => {
+    const { notification, pressAction } = detail;
+    if (type === EventType.PRESS) {
+        const link = Linking.createURL(detail.notification?.data?.url);
+        Linking.openURL(link ?? 'gorakuplus://user');
+        await notifee.cancelNotification(notification.id);
+    }
+});
+
+TaskManager.defineTask('Notifs', async () => {
+    const { enabled } = store.getState().persistedNotifs;
+    const { mediaLanguage } = store.getState().persistedSettings;
+    const fetchNotifs = store.dispatch(
+        api.endpoints.GetNotifications.initiate({ amount: 50, page: 1, reset: true }),
+    );
+    try {
+        const response = await fetchNotifs.unwrap();
+        const newNotifs = response.Page?.notifications
+            ?.slice(0, response.Viewer?.unreadNotificationCount ?? 0)
+            ?.filter((notif) => enabled?.includes(notif.__typename));
+
+        if (newNotifs.length === 1) {
+            const parsedData = parseNotif(mediaLanguage, newNotifs[0]);
+            displayNotification(parsedData);
+        } else if (newNotifs.length > 1) {
+            await notifee.displayNotification({
+                title: 'AniList Notifications',
+                subtitle: `${newNotifs.length} new notifications`,
+                android: {
+                    channelId: 'anilist-notifs',
+                    groupSummary: true,
+                    groupId: 'anilist-notifs-group',
+                },
+            });
+            newNotifs.forEach((notif) => {
+                const parsedData = parseNotif(mediaLanguage, notif);
+                displayNotification({ ...parsedData, group: true });
+            });
+        } else {
+            return BackgroundFetch.BackgroundFetchResult.NoData;
+        }
+    } catch (err) {
+        return BackgroundFetch.BackgroundFetchResult.NoData;
+    }
+
+    // Be sure to return the successful result type!
+    return BackgroundFetch.BackgroundFetchResult.NewData;
+});
+
 const App = () => {
-    const { width, height } = useWindowDimensions();
+    useEffect(() => {
+        return notifee.onForegroundEvent(({ type, detail }) => {
+            switch (type) {
+                case EventType.PRESS:
+                    const link = Linking.createURL(detail.notification?.data?.url);
+                    Linking.openURL(link ?? 'gorakuplus://user');
+                    break;
+            }
+        });
+    }, []);
+
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
             <AppProvider>
---
+++ src/features/more/settings/notifications/index.tsx
@@ -0,0 +1,211 @@
+import { ScrollView, View } from 'react-native';
+import { Button, Divider, List, Portal, Switch, Text } from 'react-native-paper';
+import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
+import * as BackgroundFetch from 'expo-background-fetch';
+import notifee from '@notifee/react-native';
+import {
+    NotifTypes,
+    addEnabledNotif,
+    disableAllNotifs,
+    enableAllNotifs,
+    removeEnabledNotif,
+    setNotifInterval,
+    setRegisteredState,
+} from './notifSlice';
+import {
+    displayNotification,
+    registerBGFetch,
+    testAnilistFetchNotif,
+    unregisterBGFetch,
+} from './backgroundFetch';
+import * as Linking from 'expo-linking';
+import { useLinkTo } from '@react-navigation/native';
+import { GetNotificationsQuery } from '../../../../app/services/anilist/generated-anilist';
+import { ListSubheader } from '../../../../components/titles';
+import { useCallback, useEffect, useState } from 'react';
+import { FetchIntervalDialog } from './components/dialog';
+
+type NotifOption = {
+    type: GetNotificationsQuery['Page']['notifications'][0]['__typename'];
+    title: string;
+};
+
+const notifMedia: NotifOption[] = [
+    {
+        type: 'AiringNotification',
+        title: 'Airing',
+    },
+    {
+        type: 'MediaDataChangeNotification',
+        title: 'Media Data Changes',
+    },
+    {
+        type: 'MediaDeletionNotification',
+        title: 'Media Deletions',
+    },
+    {
+        type: 'MediaMergeNotification',
+        title: 'Media Merges',
+    },
+    {
+        type: 'RelatedMediaAdditionNotification',
+        title: 'Related Media Additions',
+    },
+];
+
+const notifActivity: NotifOption[] = [
+    {
+        type: 'ActivityLikeNotification',
+        title: 'Activity Likes',
+    },
+    {
+        type: 'ActivityMentionNotification',
+        title: 'Activity Mentions',
+    },
+    {
+        type: 'ActivityMessageNotification',
+        title: 'Activity Messages',
+    },
+    {
+        type: 'ActivityReplyNotification',
+        title: 'Activity Replies',
+    },
+];
+const notifFollow: NotifOption[] = [
+    {
+        type: 'FollowingNotification',
+        title: 'Follows',
+    },
+];
+
+const NotifScreen = () => {
+    const [intervalVis, setIntervalVis] = useState(false);
+
+    const { enabled, fetchInterval, isRegistered } = useAppSelector(
+        (state) => state.persistedNotifs,
+    );
+    const dispatch = useAppDispatch();
+
+    const toggleNotifications = useCallback(
+        (value: boolean) => {
+            if (value) {
+                // dispatch(enableAllNotifs());
+                dispatch(setRegisteredState(true));
+                registerBGFetch(fetchInterval);
+            } else {
+                unregisterBGFetch();
+                // dispatch(disableAllNotifs());
+                dispatch(setRegisteredState(false));
+            }
+        },
+        [fetchInterval],
+    );
+
+    const updateFetchInterval = useCallback((hour: number) => {
+        dispatch(setNotifInterval(hour));
+        registerBGFetch(hour);
+    }, []);
+
+    const updateEnabled = useCallback((type: NotifTypes, add: boolean) => {
+        if (add) {
+            dispatch(addEnabledNotif(type));
+        } else {
+            dispatch(removeEnabledNotif(type));
+        }
+    }, []);
+
+    const openIntervalDialog = useCallback(() => setIntervalVis(true), []);
+    const dismissIntervalDialog = useCallback(() => setIntervalVis(false), []);
+
+    const NotifSwitch = useCallback(
+        (type: NotifOption, idx: number) => {
+            return (
+                <List.Item
+                    key={idx}
+                    title={type.title}
+                    right={() => (
+                        <Switch
+                            value={enabled?.includes(type.type)}
+                            onValueChange={(value) => updateEnabled(type.type, value)}
+                            disabled={!isRegistered}
+                        />
+                    )}
+                />
+            );
+        },
+        [enabled, isRegistered],
+    );
+
+    // const registerBackgroundFetchAsync = async () => {
+    //     await notifee.requestPermission();
+    //     dispatch(setRegisteredState(true));
+    //     return BackgroundFetch.registerTaskAsync('Notifs', {
+    //         minimumInterval: 60 * 1, // 2 minutes
+    //         stopOnTerminate: false, // android only
+    //         startOnBoot: true, // android only
+    //     });
+    // };
+
+    // const unregisterBackgroundFetchAsync = async () => {
+    //     dispatch(setRegisteredState(false));
+    //     return BackgroundFetch.unregisterTaskAsync('Notifs');
+    // };
+
+    // const testNotif = async () => {
+    //     await notifee.requestPermission();
+    //     testAnilistFetchNotif();
+    // };
+
+    return (
+        <ScrollView>
+            {/* <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                <Text>Is Registered?: </Text>
+                <Text style={{ color: isRegistered ? 'green' : 'red' }}>
+                    {isRegistered ? 'True' : 'False'}
+                </Text>
+            </View>
+            <Button onPress={registerBackgroundFetchAsync}>Register</Button>
+            <Button onPress={unregisterBackgroundFetchAsync}>Unregister</Button>
+            <Button onPress={testNotif}>Test Notif</Button>
+            <Divider bold /> */}
+            <List.Item
+                title="Allow Notifications"
+                right={() => <Switch value={isRegistered} onValueChange={toggleNotifications} />}
+                // descriptionStyle={{ textTransform: 'capitalize' }}
+            />
+            <List.Item
+                title="Toggle All Notifications"
+                onPress={() =>
+                    enabled.length > 0 ? dispatch(disableAllNotifs()) : dispatch(enableAllNotifs())
+                }
+                // descriptionStyle={{ textTransform: 'capitalize' }}
+            />
+            <List.Item
+                title="Check Frequency"
+                description={'# of hours before checking notifications again'}
+                right={() => <Text>{fetchInterval}</Text>}
+                onPress={openIntervalDialog}
+                // descriptionStyle={{ textTransform: 'capitalize' }}
+            />
+            <Divider />
+            <List.Section>
+                <ListSubheader title={'Follows'} />
+                {notifFollow.map(NotifSwitch)}
+                <ListSubheader title={'Media'} />
+                {notifMedia.map(NotifSwitch)}
+                <ListSubheader title={'Activity'} />
+                {notifActivity.map(NotifSwitch)}
+            </List.Section>
+            <Portal>
+                <FetchIntervalDialog
+                    visible={intervalVis}
+                    onDismiss={dismissIntervalDialog}
+                    initialInterval={fetchInterval}
+                    updateInterval={updateFetchInterval}
+                />
+            </Portal>
+        </ScrollView>
+    );
+};
+
+export default NotifScreen;
