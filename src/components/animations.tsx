02d4c65ee707bedc3fe67b8b8970cb33156e9b3f
Smashinfries <jus10luka@gmail.com>
2023-09-17 18:21:29 -0500

    add animated height components

--- src/components/animations.tsx
+++ src/components/animations.tsx
@@ -1,6 +1,7 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { MotiView, useDynamicAnimation } from 'moti';
-import { useRef } from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { ReactNode, memo, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import {
     View,
     StyleSheet,
@@ -8,8 +9,9 @@ import {
     StyleProp,
     ViewStyle,
     useWindowDimensions,
+    TextStyle,
 } from 'react-native';
-import { useTheme } from 'react-native-paper';
+import { Button, IconButton, List, Text, TouchableRipple, useTheme } from 'react-native-paper';
 import Animated, {
     useSharedValue,
     useAnimatedScrollHandler,
@@ -18,16 +20,23 @@ import Animated, {
     Extrapolation,
     interpolateColor,
     useDerivedValue,
+    withSpring,
 } from 'react-native-reanimated';
+import { rgbToRgba } from '../utils';
 
-export const useHeaderAnim = () => {
+export const useHeaderAnim = (start = 40, end = 110) => {
+    const input_range = [start, end];
     const { colors } = useTheme();
+    const rgbaColor = useMemo(
+        () => rgbToRgba(colors.elevation.level3, 0.85),
+        [colors.elevation.level3],
+    );
     const scrollY = useSharedValue(0);
     const scrollHandler = useAnimatedScrollHandler((event) => {
         scrollY.value = event.contentOffset.y;
     });
     const headerTitleStyle = useAnimatedStyle(() => {
-        const opac = interpolate(scrollY.value, [40, 110], [0, 1], {
+        const opac = interpolate(scrollY.value, input_range, [0, 1], {
             extrapolateLeft: Extrapolation.CLAMP,
         });
 
@@ -37,121 +46,31 @@ export const useHeaderAnim = () => {
     });
 
     const headerStyle = useAnimatedStyle(() => {
-        const bgColor = interpolateColor(
-            scrollY.value,
-            [40, 110],
-            ['transparent', colors.background],
-        );
+        const bgColor = interpolateColor(scrollY.value, input_range, ['transparent', rgbaColor]);
 
         return {
             backgroundColor: bgColor,
         };
     });
 
-    return { scrollHandler, headerStyle, headerTitleStyle };
-};
+    const headerActionStyle = useAnimatedStyle(() => {
+        const bgColor = interpolateColor(scrollY.value, input_range, [rgbaColor, 'transparent']);
 
-type AnimateHeightProps = {
-    children?: React.ReactNode;
-    initialHeight: number;
-    hide?: boolean;
-    onHeightDidAnimate?: (height: number) => void;
-    enterFrom?: 'bottom' | 'top';
-} & React.ComponentProps<typeof MotiView>;
-
-export const AnimateHeight = ({
-    initialHeight,
-    children,
-    onHeightDidAnimate,
-    enterFrom = 'top',
-    hide = false,
-    delay = Platform.select({ web: 250, default: 0 }),
-    transition = { type: 'timing', delay },
-    style,
-    ...motiViewProps
-}: AnimateHeightProps) => {
-    const measuredHeight = useSharedValue(initialHeight);
-    const { colors } = useTheme();
-    const state = useDynamicAnimation(() => {
         return {
-            height: initialHeight,
-            // opacity: !initialHeight || hide ? 0 : 1,
+            backgroundColor: bgColor,
         };
     });
 
-    if ('state' in motiViewProps) {
-        console.warn('[AnimateHeight] state prop not supported');
-    }
-
-    const animation = useDerivedValue(() => {
-        let height = Math.ceil(measuredHeight.value);
-        if (hide) {
-            height = 80;
-        }
-
-        const notVisible = !height || hide;
-
-        state.animateTo({
-            height,
-            // opacity: !height || hide ? 0 : 1,
-        });
-    }, [hide, measuredHeight]);
-
-    return (
-        <MotiView
-            {...motiViewProps}
-            state={state}
-            // animate={{ height: initialHeight }}
-            transition={transition}
-            onDidAnimate={
-                onHeightDidAnimate &&
-                ((key, finished, _, { attemptedValue }) =>
-                    key == 'height' && onHeightDidAnimate(attemptedValue as number))
-            }
-            style={[styles.hidden, style]}
-        >
-            <View
-                style={[
-                    StyleSheet.absoluteFill,
-                    styles.autoBottom,
-
-                    // THIS BREAKS IDK WHY, so ignore that prop
-                    // enterFrom === 'top' ? styles.autoBottom : styles.autoTop,
-                ]}
-                onLayout={({ nativeEvent }) => {
-                    measuredHeight.value = nativeEvent.layout.height;
-                }}
-            >
-                {children}
-            </View>
-            <LinearGradient
-                colors={['transparent', colors.background]}
-                locations={hide ? [0.5, 1] : [1, 1]}
-                style={{ position: 'absolute', height: '100%', width: '100%' }}
-            />
-        </MotiView>
-    );
+    return { scrollHandler, headerStyle, headerTitleStyle, headerActionStyle };
 };
-
-const styles = StyleSheet.create({
-    autoBottom: {
-        bottom: 'auto',
-        paddingBottom: 10,
-    },
-    autoTop: {
-        top: 'auto',
-    },
-    hidden: {
-        overflow: 'hidden',
-    },
-});
 
 type AnimViewProps = {
     style?: StyleProp<ViewStyle>;
     animation?: boolean;
+    delay?: number;
     children: React.ReactNode;
 };
-export const TransYUpView = ({ children, style, animation = true }: AnimViewProps) => {
+export const TransYUpView = ({ children, style, delay, animation = true }: AnimViewProps) => {
     const { height } = useWindowDimensions();
 
     if (!animation) {
@@ -163,6 +82,7 @@ export const TransYUpView = ({ children, style, animation = true }: AnimViewProp
             style={style}
             from={{ translateY: height }}
             animate={{ translateY: 0 }}
+            delay={delay}
             transition={{ type: 'spring', mass: 0.5, damping: 10 }}
         >
             {children}
@@ -174,16 +94,242 @@ export const TransXInView = ({
     children,
     style,
     direction,
+    delay,
 }: AnimViewProps & { direction: 'left' | 'right' }) => {
     const { width } = useWindowDimensions();
     return (
         <MotiView
+            renderToHardwareTextureAndroid
             style={style}
             from={{ translateX: direction === 'left' ? -width : width }}
             animate={{ translateX: direction === 'right' ? 0 : 0 }}
+            delay={delay}
             transition={{ type: 'spring', mass: 0.5, damping: 10 }}
         >
             {children}
         </MotiView>
+    );
+};
+
+export const TransXInViewMem = memo(TransXInView);
+export const TransYUpViewMem = memo(TransYUpView);
+
+type ToggableChevronProps = {
+    isExpanded: boolean;
+};
+const ToggableChevron = ({ isExpanded }: ToggableChevronProps) => {
+    const iconRotation = useSharedValue(0);
+    const { colors } = useTheme();
+
+    const animatedIconStyle = useAnimatedStyle(() => {
+        return {
+            transform: [{ rotate: `${iconRotation.value}deg` }],
+        };
+    });
+
+    const toggleRotation = useCallback(() => {
+        iconRotation.value = withSpring(isExpanded ? 0 : 180, { damping: 10, mass: 0.5 });
+    }, [iconRotation, isExpanded]);
+
+    useEffect(() => {
+        toggleRotation();
+    }, [isExpanded, toggleRotation]);
+
+    return (
+        <Animated.View style={[animatedIconStyle]}>
+            <MaterialCommunityIcons size={24} color={colors.onSurfaceVariant} name={'chevron-up'} />
+        </Animated.View>
+    );
+};
+
+type AnimateHeightProps = {
+    initialHeight: number;
+    children: ReactNode;
+};
+export const ExpandableDescription = ({ initialHeight, children }: AnimateHeightProps) => {
+    const { colors } = useTheme();
+    const height = useSharedValue(initialHeight);
+    const [totalHeight, setTotalHeight] = useState<number>(0);
+    const [currentHeight, setCurrentHeight] = useState<number>(initialHeight);
+    const animatedStyles = useAnimatedStyle(() => {
+        return {
+            height: height.value,
+        };
+    });
+
+    const toggleHeight = useCallback(() => {
+        height.value = withSpring(
+            height.value === totalHeight
+                ? initialHeight
+                : totalHeight - height.value + initialHeight,
+            { damping: 10, mass: 0.5 },
+        );
+        setCurrentHeight(
+            height.value === totalHeight
+                ? initialHeight
+                : totalHeight - height.value + initialHeight,
+        );
+    }, [height, totalHeight]);
+
+    return (
+        <View style={{ overflow: 'visible', marginVertical: 15 }}>
+            <Animated.View style={[animatedStyles, { overflow: 'hidden' }]}>
+                <View style={[StyleSheet.absoluteFill, { bottom: 'auto', paddingBottom: 10 }]}>
+                    <View
+                        onLayout={(e) => setTotalHeight(e.nativeEvent.layout.height)}
+                        style={{
+                            paddingHorizontal: 20,
+                            paddingVertical: 10,
+                            paddingBottom: 20,
+                            // backgroundColor: colors.secondaryContainer,
+                            borderRadius: 12,
+                            margin: 15,
+                        }}
+                    >
+                        {children}
+                    </View>
+                </View>
+                <LinearGradient
+                    colors={['transparent', colors.background]}
+                    locations={
+                        Math.floor(currentHeight) < Math.floor(totalHeight) ? [0.5, 1] : [1, 1]
+                    }
+                    style={{ position: 'absolute', height: '100%', width: '100%' }}
+                />
+            </Animated.View>
+            {currentHeight <= totalHeight && (
+                <IconButton
+                    icon={
+                        Math.floor(currentHeight) === initialHeight ? 'chevron-down' : 'chevron-up'
+                    }
+                    onPress={toggleHeight}
+                    style={{
+                        position: 'absolute',
+                        bottom: -35,
+                        alignSelf: 'center',
+                        overflow: 'visible',
+                    }}
+                />
+            )}
+        </View>
+    );
+};
+
+type AccordianProps = {
+    title: string;
+    titleNumberOfLines?: number;
+    description?: string;
+    descriptionNumberOfLines?: number;
+    descriptionStyle?: StyleProp<TextStyle>;
+    children: ReactNode;
+    initialExpand?: boolean;
+};
+export const Accordian = ({
+    title,
+    titleNumberOfLines,
+    children,
+    description,
+    descriptionNumberOfLines,
+    descriptionStyle,
+    initialExpand = false,
+}: AccordianProps) => {
+    const { colors } = useTheme();
+    const [isExpanded, setIsExpanded] = useState(initialExpand);
+    const initialHeight = 0;
+    const height = useSharedValue(0);
+    const [totalHeight, setTotalHeight] = useState<number>(0);
+    const [currentHeight, setCurrentHeight] = useState<number>(initialHeight);
+    const animatedStyles = useAnimatedStyle(() => {
+        return {
+            height: height.value,
+        };
+    });
+
+    const toggleHeight = useCallback(() => {
+        height.value = withSpring(
+            height.value === totalHeight
+                ? initialHeight
+                : totalHeight - height.value + initialHeight,
+            { damping: 10, mass: 0.5 },
+        );
+        setCurrentHeight(
+            height.value === totalHeight
+                ? initialHeight
+                : totalHeight - height.value + initialHeight,
+        );
+        setIsExpanded((prev) => !prev);
+    }, [height, totalHeight]);
+
+    useEffect(() => {
+        if (initialExpand) {
+            toggleHeight();
+        }
+    }, [initialExpand, totalHeight]);
+
+    return (
+        <View style={{ overflow: 'visible' }}>
+            <View style={{ backgroundColor: colors?.background }}>
+                <TouchableRipple
+                    onPress={toggleHeight}
+                    rippleColor={colors.background}
+                    borderless
+                    style={{ paddingVertical: 8, paddingRight: 24 }}
+                >
+                    <View style={{ flexDirection: 'row', marginVertical: 6 }}>
+                        <View style={[{ paddingLeft: 16 }, { flex: 1, justifyContent: 'center' }]}>
+                            <Text
+                                selectable={false}
+                                numberOfLines={titleNumberOfLines}
+                                style={[{ fontSize: 16 }]}
+                            >
+                                {title}
+                            </Text>
+                            {description ? (
+                                <Text
+                                    selectable={false}
+                                    numberOfLines={descriptionNumberOfLines}
+                                    style={[
+                                        {
+                                            fontSize: 14,
+                                            color: colors.onSurfaceVariant,
+                                        },
+                                        descriptionStyle,
+                                    ]}
+                                >
+                                    {description}
+                                </Text>
+                            ) : null}
+                        </View>
+                        <View style={[{ marginVertical: 6, paddingLeft: 8 }]}>
+                            {/* <Animated.View>
+                                <MaterialCommunityIcons
+                                    size={24}
+                                    color={colors.onSurfaceVariant}
+                                    name={isExpanded ? 'chevron-up' : 'chevron-down'}
+                                />
+                            </Animated.View> */}
+                            <ToggableChevron isExpanded={isExpanded} />
+                        </View>
+                    </View>
+                </TouchableRipple>
+            </View>
+            <Animated.View style={[animatedStyles, { overflow: 'hidden' }]}>
+                <View style={[StyleSheet.absoluteFill, { bottom: 'auto', paddingBottom: 10 }]}>
+                    <View
+                        onLayout={(e) => setTotalHeight(e.nativeEvent.layout.height)}
+                        // style={{
+                        //     paddingHorizontal: 20,
+                        //     paddingVertical: 10,
+                        //     paddingBottom: 20,
+                        //     // backgroundColor: colors.secondaryContainer,
+                        //     borderRadius: 12,
+                        //     margin: 15,
+                        // }}
+                    >
+                        {children}
+                    </View>
+                </View>
+            </Animated.View>
+        </View>
     );
 };
