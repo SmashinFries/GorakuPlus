8c546b78a3ccfa3ebf84268f8058d0bf08f06bf1
Smashinfries <jus10luka@gmail.com>
2023-09-17 18:41:11 -0500

    remake hooks

--- src/features/explore/hooks/data.ts
+++ src/features/explore/hooks/data.ts
@@ -1,164 +1,329 @@
 import { useEffect, useState } from 'react';
-import {
-    useLazyAnimeNextSeasonQuery,
-    useLazyAnimePopularQuery,
-    useLazyAnimeThisSeasonQuery,
-    useLazyAnimeTopScoredQuery,
-    useLazyAnimeTrendingQuery,
-    useLazyMangaPopularQuery,
-    useLazyMangaTopScoredQuery,
-    useLazyMangaTrendingQuery,
-    useLazyManhwaPopularQuery,
-    useLazyManhwaTopScoredQuery,
-    useLazyManhwaTrendingQuery,
-    useLazyNovelPopularQuery,
-    useLazyNovelTopScoredQuery,
-    useLazyNovelTrendingQuery,
-} from '../../../app/services/anilist/generated-anilist';
 import { getSeason } from '../helpers/helpers';
+import {
+    useAnimeNextSeasonQuery,
+    useAnimePopularQuery,
+    useAnimeThisSeasonQuery,
+    useAnimeTopScoredQuery,
+    useMangaTrendingQuery,
+    useMangaPopularQuery,
+    useMangaTopScoredQuery,
+    useManhwaTrendingQuery,
+    useManhwaPopularQuery,
+    useManhwaTopScoredQuery,
+    useNovelTrendingQuery,
+    useNovelPopularQuery,
+    useNovelTopScoredQuery,
+    useAnimeTrendingQuery,
+} from '../../../app/services/anilist/enhanced';
+import { useAppSelector } from '../../../app/hooks';
 
 export const useAnimeExplorer = () => {
     const thisSeasonParams = getSeason();
     const nextSeasonParams = getSeason(true);
+    const { showNSFW, globalTagExclude } = useAppSelector((state) => state.persistedSettings);
 
-    // { page: 1, perPage: 25 }
-    const [trendTrig, trendResults, lastPromiseInfo] = useLazyAnimeTrendingQuery();
-    const [curSeasonTrig, curSeasonResults] = useLazyAnimeThisSeasonQuery();
-    const [nxtSeasonTrig, nxtSeasonResults] = useLazyAnimeNextSeasonQuery();
-    const [popularTrig, popularResults] = useLazyAnimePopularQuery();
-    const [topTrig, topResults] = useLazyAnimeTopScoredQuery();
+    const [skip, setSkip] = useState(true);
+    const [trendPage, setTrendPage] = useState(1);
+    const [cSeasonPage, setCSeasonPage] = useState(1);
+    const [nSeasonPage, setNSeasonPage] = useState(1);
+    const [popularPage, setPopularPage] = useState(1);
+    const [scorePage, setScorePage] = useState(1);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const getParams = (page: number) => {
+        return {
+            page: page,
+            perPage: 30,
+            isAdult: showNSFW ? undefined : false,
+            tag_not_in: globalTagExclude ?? undefined,
+        };
+    };
+
+    const trend = useAnimeTrendingQuery(getParams(trendPage), { skip: skip });
+    const curSeason = useAnimeThisSeasonQuery(
+        {
+            page: cSeasonPage,
+            perPage: 25,
+            season: thisSeasonParams.current_season,
+            seasonYear: thisSeasonParams.year,
+            isAdult: showNSFW ? undefined : false,
+        },
+        { skip: skip },
+    );
+    const nxtSeason = useAnimeNextSeasonQuery(
+        {
+            page: nSeasonPage,
+            perPage: 25,
+            season: nextSeasonParams.current_season,
+            seasonYear: nextSeasonParams.year,
+            isAdult: showNSFW ? undefined : false,
+        },
+        { skip: skip },
+    );
+    const popular = useAnimePopularQuery(getParams(popularPage), { skip: skip });
+    const top = useAnimeTopScoredQuery(getParams(scorePage), { skip: skip });
     const [isError, setIsError] = useState(false);
 
     const fetchAnime = (refetch = false) => {
-        trendTrig({ page: 1, perPage: 25 }, !refetch);
-        curSeasonTrig(
-            {
-                page: 1,
-                perPage: 25,
-                season: thisSeasonParams.current_season,
-                seasonYear: thisSeasonParams.year,
-            },
-            !refetch,
-        );
-        nxtSeasonTrig(
-            {
-                page: 1,
-                perPage: 25,
-                season: nextSeasonParams.current_season,
-                seasonYear: nextSeasonParams.year,
-            },
-            !refetch,
-        );
-        popularTrig({ page: 1, perPage: 25 }, !refetch);
-        topTrig({ page: 1, perPage: 25 }, !refetch);
+        setSkip(false);
+        if (refetch) {
+            trend.refetch();
+            curSeason.refetch();
+            nxtSeason.refetch();
+            popular.refetch();
+            top.refetch();
+        }
+    };
+
+    const fetchMore = (category: 'trending' | 'c_season' | 'n_season' | 'popular' | 'score') => {
+        switch (category) {
+            case 'trending':
+                setTrendPage((prev) => prev + 1);
+                break;
+            case 'c_season':
+                setCSeasonPage((prev) => prev + 1);
+                break;
+            case 'n_season':
+                setNSeasonPage((prev) => prev + 1);
+                break;
+            case 'popular':
+                setPopularPage((prev) => prev + 1);
+                break;
+            case 'score':
+                setScorePage((prev) => prev + 1);
+                break;
+        }
     };
 
     useEffect(() => {
         if (
-            trendResults.isError ||
-            curSeasonResults.isError ||
-            nxtSeasonResults.isError ||
-            popularResults.isError ||
-            topResults.isError
+            trend.isError ||
+            curSeason.isError ||
+            nxtSeason.isError ||
+            popular.isError ||
+            top.isError
         ) {
+            console.log(trend.error);
             setIsError(true);
         }
+    }, [trend.isError, curSeason.isError, nxtSeason.isError, popular.isError, top.isError]);
+
+    useEffect(() => {
+        if (
+            !trend.isUninitialized &&
+            !curSeason.isUninitialized &&
+            !nxtSeason.isUninitialized &&
+            !popular.isUninitialized &&
+            !top.isUninitialized
+        ) {
+            if (
+                !trend.isFetching &&
+                !curSeason.isFetching &&
+                !nxtSeason.isFetching &&
+                !popular.isFetching &&
+                !top.isFetching
+            ) {
+                setIsLoading(false);
+            }
+        }
     }, [
-        trendResults.isError,
-        curSeasonResults.isError,
-        nxtSeasonResults.isError,
-        popularResults.isError,
-        topResults.isError,
+        trend.isFetching,
+        curSeason.isFetching,
+        nxtSeason.isFetching,
+        popular.isFetching,
+        top.isFetching,
     ]);
 
     return {
-        trendResults,
-        curSeasonResults,
-        nxtSeasonResults,
-        popularResults,
-        topResults,
+        trendResults: trend,
+        curSeasonResults: curSeason,
+        nxtSeasonResults: nxtSeason,
+        popularResults: popular,
+        topResults: top,
         isError,
+        isLoading,
         fetchAnime,
+        fetchMore,
     };
 };
 
 export const useMangaExplorer = () => {
-    const [trendTrig, trendResults] = useLazyMangaTrendingQuery();
-    const [popularTrig, popularResults] = useLazyMangaPopularQuery();
-    const [topTrig, topResults] = useLazyMangaTopScoredQuery();
+    const { showNSFW, globalTagExclude } = useAppSelector((state) => state.persistedSettings);
+    const [skip, setSkip] = useState(true);
+    const [trendPage, setTrendPage] = useState(1);
+    const [popularPage, setPopularPage] = useState(1);
+    const [topPage, setTopPage] = useState(1);
+
+    const getParams = (page: number) => {
+        return {
+            page: page,
+            perPage: 30,
+            isAdult: showNSFW ? undefined : false,
+            tag_not_in: globalTagExclude ?? undefined,
+        };
+    };
+
+    const trend = useMangaTrendingQuery(getParams(trendPage), { skip: skip });
+    const popular = useMangaPopularQuery(getParams(popularPage), { skip: skip });
+    const top = useMangaTopScoredQuery(getParams(topPage), { skip: skip });
     const [isError, setIsError] = useState(false);
 
     const fetchManga = (refetch = false) => {
-        trendTrig({ page: 1, perPage: 25 }, !refetch);
-        popularTrig({ page: 1, perPage: 25 }, !refetch);
-        topTrig({ page: 1, perPage: 25 }, !refetch);
+        setSkip(false);
+        if (refetch) {
+            trend.refetch();
+            popular.refetch();
+            top.refetch();
+        }
+    };
+
+    const fetchMore = (category: 'trending' | 'popular' | 'score') => {
+        switch (category) {
+            case 'trending':
+                setTrendPage((prev) => prev + 1);
+                break;
+            case 'popular':
+                setPopularPage((prev) => prev + 1);
+                break;
+            case 'score':
+                setTopPage((prev) => prev + 1);
+                break;
+        }
     };
 
     useEffect(() => {
-        if (trendResults.isError || popularResults.isError || topResults.isError) {
+        if (trend.isError || popular.isError || top.isError) {
             setIsError(true);
         }
-    }, [trendResults.isError, popularResults.isError, topResults.isError]);
+    }, [trend.isError, popular.isError, top.isError]);
 
     return {
-        trendResults,
-        popularResults,
-        topResults,
+        trendResults: trend,
+        popularResults: popular,
+        topResults: top,
         isError,
         fetchManga,
+        fetchMore,
     };
 };
 
 export const useManhwaExplorer = () => {
-    const [trendTrig, trendResults] = useLazyManhwaTrendingQuery();
-    const [popularTrig, popularResults] = useLazyManhwaPopularQuery();
-    const [topTrig, topResults] = useLazyManhwaTopScoredQuery();
+    const { showNSFW, globalTagExclude } = useAppSelector((state) => state.persistedSettings);
+    const [skip, setSkip] = useState(true);
+    const [trendPage, setTrendPage] = useState(1);
+    const [popularPage, setPopularPage] = useState(1);
+    const [topPage, setTopPage] = useState(1);
     const [isError, setIsError] = useState(false);
 
+    const getParams = (page: number) => {
+        return {
+            page: page,
+            perPage: 30,
+            isAdult: showNSFW ? undefined : false,
+            tag_not_in: globalTagExclude ?? undefined,
+        };
+    };
+
+    const trend = useManhwaTrendingQuery(getParams(trendPage), { skip: skip });
+    const popular = useManhwaPopularQuery(getParams(popularPage), { skip: skip });
+    const top = useManhwaTopScoredQuery(getParams(topPage), { skip: skip });
+
     const fetchManhwa = (refetch = false) => {
-        trendTrig({ page: 1, perPage: 25 }, !refetch);
-        popularTrig({ page: 1, perPage: 25 }, !refetch);
-        topTrig({ page: 1, perPage: 25 }, !refetch);
+        setSkip(false);
+        if (refetch) {
+            trend.refetch();
+            popular.refetch();
+            top.refetch();
+        }
+    };
+
+    const fetchMore = (category: 'trending' | 'popular' | 'score') => {
+        switch (category) {
+            case 'trending':
+                setTrendPage((prev) => prev + 1);
+                break;
+            case 'popular':
+                setPopularPage((prev) => prev + 1);
+                break;
+            case 'score':
+                setTopPage((prev) => prev + 1);
+                break;
+        }
     };
 
     useEffect(() => {
-        if (trendResults.isError || popularResults.isError || topResults.isError) {
+        if (trend.isError || popular.isError || top.isError) {
             setIsError(true);
         }
-    }, [trendResults.isError, popularResults.isError, topResults.isError]);
+    }, [trend.isError, popular.isError, top.isError]);
 
     return {
-        trendResults,
-        popularResults,
-        topResults,
+        trendResults: trend,
+        popularResults: popular,
+        topResults: top,
         isError,
         fetchManhwa,
+        fetchMore,
     };
 };
 
 export const useNovelExplorer = () => {
-    const [trendTrig, trendResults] = useLazyNovelTrendingQuery();
-    const [popularTrig, popularResults] = useLazyNovelPopularQuery();
-    const [topTrig, topResults] = useLazyNovelTopScoredQuery();
+    const { showNSFW, globalTagExclude } = useAppSelector((state) => state.persistedSettings);
+    const [skip, setSkip] = useState(true);
+    const [trendPage, setTrendPage] = useState(1);
+    const [popularPage, setPopularPage] = useState(1);
+    const [topPage, setTopPage] = useState(1);
     const [isError, setIsError] = useState(false);
 
+    const getParams = (page: number) => {
+        return {
+            page: page,
+            perPage: 30,
+            isAdult: showNSFW ? undefined : false,
+            tag_not_in: globalTagExclude ?? undefined,
+        };
+    };
+
+    const trend = useNovelTrendingQuery(getParams(trendPage), { skip: skip });
+    const popular = useNovelPopularQuery(getParams(popularPage), { skip: skip });
+    const top = useNovelTopScoredQuery(getParams(topPage), { skip: skip });
+
     const fetchNovels = (refetch = false) => {
-        trendTrig({ page: 1, perPage: 25 }, !refetch);
-        popularTrig({ page: 1, perPage: 25 }, !refetch);
-        topTrig({ page: 1, perPage: 25 }, !refetch);
+        setSkip(false);
+        if (refetch) {
+            trend.refetch();
+            popular.refetch();
+            top.refetch();
+        }
+    };
+
+    const fetchMore = (category: 'trending' | 'popular' | 'score') => {
+        switch (category) {
+            case 'trending':
+                setTrendPage((prev) => prev + 1);
+                break;
+            case 'popular':
+                setPopularPage((prev) => prev + 1);
+                break;
+            case 'score':
+                setTopPage((prev) => prev + 1);
+                break;
+        }
     };
 
     useEffect(() => {
-        if (trendResults.isError || popularResults.isError || topResults.isError) {
+        if (trend.isError || popular.isError || top.isError) {
             setIsError(true);
         }
-    }, [trendResults.isError, popularResults.isError, topResults.isError]);
+    }, [trend.isError, popular.isError, top.isError]);
 
     return {
-        trendResults,
-        popularResults,
-        topResults,
+        trendResults: trend,
+        popularResults: popular,
+        topResults: top,
         isError,
         fetchNovels,
+        fetchMore,
     };
 };
