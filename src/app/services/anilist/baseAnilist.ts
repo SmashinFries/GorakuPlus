bee6a828b2656e9fd0090e1d42b967cc760cd409
Smashinfries <jus10luka@gmail.com>
2023-09-17 18:15:06 -0500

    remove log

--- src/app/services/anilist/baseAnilist.ts
+++ src/app/services/anilist/baseAnilist.ts
@@ -10,7 +10,6 @@ export const api = createApi({
         client,
         prepareHeaders: (headers, { getState }) => {
             const { token } = (getState() as RootState).persistedAniLogin;
-            // console.log('token', token);
             if (token) {
                 headers.set('authorization', `Bearer ${token}`);
             }
