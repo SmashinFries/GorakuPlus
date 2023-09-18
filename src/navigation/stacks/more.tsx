653aaa6b715c6cc2248d2329431c81873d35606f
Smashinfries <jus10luka@gmail.com>
2023-09-17 20:19:54 -0500

    add stacks

--- src/navigation/stacks/more.tsx
+++ src/navigation/stacks/more.tsx
@@ -4,16 +4,25 @@ import PaperHeader, { MoreHeader } from '../../components/headers';
 import SettingsScreen from '../../features/more/settings';
 import MoreScreen from '../../features/more';
 import AccountsScreen from '../../features/more/accounts';
-import { MoreStackProps, SettingsStackProps } from '../types';
+import {
+    BannedTagsStackProps,
+    MediaSettingsStackProps,
+    MoreStackProps,
+    SettingsStackProps,
+} from '../types';
 import AppearanceScreen from '../../features/more/settings/appearance';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../app/store';
-import DataSettingsScreen from '../../features/more/settings/tabs';
 import LanguageScreen from '../../features/more/settings/language';
 import MediaSettingScreen from '../../features/more/settings/media';
+import AboutScreen from '../../features/more/about';
+import NotifScreen from '../../features/more/settings/notifications';
+import TagFilterScreen from '../../features/more/settings/media/tagFilter';
 
 const MoreStackNav = createNativeStackNavigator<MoreStackProps>();
 const SettingsStackNav = createNativeStackNavigator<SettingsStackProps>();
+const MediaSettingsStackNav = createNativeStackNavigator<MediaSettingsStackProps>();
+const BannedTagsStackNav = createNativeStackNavigator<BannedTagsStackProps>();
 
 export const MoreStack = () => {
     const { navAnimation } = useSelector((state: RootState) => state.persistedSettings);
@@ -39,7 +48,55 @@ export const MoreStack = () => {
                 component={SettingsStack}
                 options={{ title: 'Settings', headerShown: false }}
             />
+            <MoreStackNav.Screen
+                name="about"
+                component={AboutScreen}
+                options={{ title: 'About', header: (props) => <PaperHeader {...props} /> }}
+            />
         </MoreStackNav.Navigator>
+    );
+};
+
+export const BannedTagsStack = () => {
+    const { navAnimation } = useSelector((state: RootState) => state.persistedSettings);
+
+    return (
+        <BannedTagsStackNav.Navigator
+            initialRouteName="btags"
+            screenOptions={{ animation: navAnimation, headerShown: true }}
+        >
+            <BannedTagsStackNav.Screen
+                name="btags"
+                component={TagFilterScreen}
+                options={{ title: 'Exclude Tags', header: (props) => <PaperHeader {...props} /> }}
+            />
+        </BannedTagsStackNav.Navigator>
+    );
+};
+
+export const MediaSettingsStack = () => {
+    const { navAnimation } = useSelector((state: RootState) => state.persistedSettings);
+
+    return (
+        <MediaSettingsStackNav.Navigator
+            initialRouteName="msHome"
+            screenOptions={{ animation: navAnimation, headerShown: false }}
+        >
+            <MediaSettingsStackNav.Screen
+                name="msHome"
+                component={MediaSettingScreen}
+                options={{
+                    title: 'Media',
+                    headerShown: true,
+                    header: (props) => <PaperHeader {...props} />,
+                }}
+            />
+            <MediaSettingsStackNav.Screen
+                name="bannedTags"
+                component={BannedTagsStack}
+                options={{ headerShown: false }}
+            />
+        </MediaSettingsStackNav.Navigator>
     );
 };
 
@@ -61,19 +118,23 @@ export const SettingsStack = () => {
                 options={{ title: 'Appearance', header: (props) => <PaperHeader {...props} /> }}
             />
             <SettingsStackNav.Screen
-                name="tabs"
-                component={DataSettingsScreen}
-                options={{ title: 'Tabs', header: (props) => <PaperHeader {...props} /> }}
-            />
-            <SettingsStackNav.Screen
                 name="language"
                 component={LanguageScreen}
                 options={{ title: 'Language', header: (props) => <PaperHeader {...props} /> }}
             />
             <SettingsStackNav.Screen
-                name="media"
-                component={MediaSettingScreen}
-                options={{ title: 'Media', header: (props) => <PaperHeader {...props} /> }}
+                name="notifications"
+                component={NotifScreen}
+                options={{ title: 'Notifications', header: (props) => <PaperHeader {...props} /> }}
+            />
+            <SettingsStackNav.Screen
+                name="mediaSettings"
+                component={MediaSettingsStack}
+                options={{
+                    title: 'Media',
+                    headerShown: false,
+                    header: (props) => <PaperHeader {...props} />,
+                }}
             />
         </SettingsStackNav.Navigator>
     );
