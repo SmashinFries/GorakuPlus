8f087f79e09aa496a74bd1801bb645026b71d791
Smashinfries <jus10luka@gmail.com>
2023-09-17 20:21:57 -0500

    add sort tools

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -9,3 +9,10 @@ export const sortTagsRank = (tags: MediaTag[]) => {
         return [];
     }
 };
+
+export const isSame = (obj1: object, obj2: object) => {
+    const obj1Keys = Object.keys(obj1);
+    const obj2Keys = Object.keys(obj2);
+
+    return obj1Keys.length === obj2Keys.length && obj1Keys.every((key) => obj1[key] === obj2[key]);
+};
