4d2369f444b601d16c994ce3796c7af746d5b91e
Smashinfries <jus10luka@gmail.com>
2023-09-17 19:54:41 -0500

    add onpress

--- src/features/media/components/quickSelect.tsx
+++ src/features/media/components/quickSelect.tsx
@@ -2,35 +2,43 @@ import { IconButton, useTheme } from 'react-native-paper';
 import { MotiView } from 'moti';
 import { Selectable } from '../../../components/moti';
 import { CrunchyRollIcon } from '../../../components/svgs';
+import { View } from 'react-native';
 
 type QuickSelectorProps = {
     icon: string;
     disabled?: boolean;
     onPress?: () => void;
 };
-export const QuickSelector = ({ icon, disabled, onPress }: QuickSelectorProps) => {
+export const QuickSelector = ({ icon, disabled = false, onPress }: QuickSelectorProps) => {
     const { colors } = useTheme();
     return (
-        <Selectable
+        <View
             style={{
-                flex: 1,
+                // flex: 1,
+                maxWidth: 34,
                 justifyContent: 'center',
                 alignItems: 'center',
+                alignSelf: 'center',
             }}
-            disabled={disabled}
-            onPress={onPress}
         >
             {icon !== 'crunchy' ? (
-                <IconButton disabled={disabled} iconColor={colors.primary} icon={icon} size={32} />
+                <IconButton
+                    onPress={onPress ?? null}
+                    disabled={disabled}
+                    iconColor={colors.primary}
+                    icon={icon}
+                    size={32}
+                />
             ) : (
                 <CrunchyRollIcon
                     width={38}
                     height={38}
+                    onPress={onPress ?? null}
                     logoColor={disabled ? colors.onSurface : colors.primary}
                     opacity={disabled ? 0.12 : 1}
                     fontColor={colors.onBackground}
                 />
             )}
-        </Selectable>
+        </View>
     );
 };
