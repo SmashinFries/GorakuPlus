ac569588e43430652708c1552ad9aa2e0fa669bf
Smashinfries <jus10luka@gmail.com>
2023-09-17 20:06:14 -0500

    add notification settings

--- src/features/more/settings/index.tsx
+++ src/features/more/settings/index.tsx
@@ -2,30 +2,27 @@ import React from 'react';
 import { ThemeOptions } from '../../../theme/theme';
 import { useDispatch, useSelector } from 'react-redux';
 import { setTheme } from '../../../theme/themeSlice';
-import { List, RadioButton, Switch, useTheme } from 'react-native-paper';
+import { List, RadioButton, Switch, Text, useTheme } from 'react-native-paper';
 import { RootState } from '../../../app/store';
 import { ScrollView } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
 import { api } from '../../../app/services/anilist/enhanced';
 import { MoreStackProps, SettingsStackProps } from '../../../navigation/types';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { useAppDispatch } from '../../../app/hooks';
 
 WebBrowser.maybeCompleteAuthSession();
 
 const SettingsScreen = ({
     navigation,
 }: NativeStackScreenProps<SettingsStackProps, 'settingsHome'>) => {
-    const dispatch = useDispatch();
     const { colors } = useTheme();
-
-    const resetCache = () =>
-        dispatch(api.util.invalidateTags(['ExploreAnime', 'ExploreManga', 'ExploreNovel']));
 
     return (
         <ScrollView>
             <List.Item
                 title={'Appearance'}
-                description={'Themes, animations'}
+                description={'Themes, animations, scoring'}
                 onPress={() => {
                     navigation.navigate('appearance');
                 }}
@@ -34,28 +31,30 @@ const SettingsScreen = ({
                 )}
             />
             <List.Item
-                title={'Tabs'}
-                description={'Order and visibility of tabs'}
-                onPress={() => {
-                    navigation.navigate('tabs');
-                }}
-                left={(props) => <List.Icon {...props} color={colors.primary} icon="tab" />}
-            />
-            <List.Item
                 title={'Media'}
-                description={'Change media defaults'}
+                description={'Media defaults'}
                 onPress={() => {
-                    navigation.navigate('media');
+                    navigation.navigate('mediaSettings');
                 }}
                 left={(props) => <List.Icon {...props} color={colors.primary} icon="television" />}
             />
             <List.Item
                 title={'Language'}
-                description={'Change app and media title langauge'}
+                description={'App and media title langauge'}
                 onPress={() => {
                     navigation.navigate('language');
                 }}
                 left={(props) => <List.Icon {...props} color={colors.primary} icon="translate" />}
+            />
+            <List.Item
+                title={'Notifications'}
+                description={'Episode updates and more'}
+                onPress={() => {
+                    navigation.navigate('notifications');
+                }}
+                left={(props) => (
+                    <List.Icon {...props} color={colors.primary} icon="bell-outline" />
+                )}
             />
             {/* <List.Item
                 title={'Animations'}
