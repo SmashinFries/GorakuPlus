5f046f50db48417504217ae7af03715e4f3950d2
Smashinfries <jus10luka@gmail.com>
2023-09-17 20:16:14 -0500

    add auth user screen

--- src/features/user/authedUser.tsx
+++ src/features/user/authedUser.tsx
@@ -1,45 +1,68 @@
 import { RefreshControl, ScrollView, View } from 'react-native';
 import {
+    useUserActivityQuery,
     useUserFollowersQuery,
     useUserFollowingQuery,
     useUserOverviewQuery,
 } from '../../app/services/anilist/generated-anilist';
-import { ActivityIndicator, useTheme } from 'react-native-paper';
+import { ActivityIndicator, Button, Portal, useTheme } from 'react-native-paper';
 import { UserBanner } from './components/banner';
 import { UserHeader } from './components/header';
 import { StatOverview } from './components/quickStats';
 import { FavOverview } from './components/favOverview';
 import { FollowRow } from './components/followOverview';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
+import { NavigationProp, NavigatorScreenParams, useNavigation } from '@react-navigation/native';
+import { RootStackProps } from '../../navigation/types';
+import { useUser } from './hooks/useUser';
+import { ActivityOverview } from './activity';
+import { FadeHeaderProvider } from '../../components/headers';
+import { AddFriendDialog } from './components/dialogs';
+import { Image } from 'expo-image';
 
 const AuthUserScreen = ({ userID }: { userID: number }) => {
-    const user = useUserOverviewQuery();
-    const followers = useUserFollowersQuery({ page: 1, userId: userID });
-    const following = useUserFollowingQuery({ page: 1, userId: userID });
+    const navigation = useNavigation<NavigationProp<RootStackProps>>();
+    const { colors } = useTheme();
+    const { activity, followers, following, user } = useUser(userID);
     const [isRefreshing, setIsRefreshing] = useState(false);
 
-    const { colors } = useTheme();
+    const [showAddFriend, setShowAddFriend] = useState(false);
 
-    // const { width, height } = useWindowDimensions();
+    const dismissAddFriend = useCallback(() => setShowAddFriend(false), []);
+
+    const navToStats = useCallback(() => {
+        navigation.navigate('statistics', { userId: userID });
+    }, []);
 
     const onRefresh = () => {
         setIsRefreshing(true);
         user.refetch();
         followers.refetch();
         following.refetch();
+        // activity.refetch();
         setIsRefreshing(false);
     };
 
-    if (user.isLoading) return <ActivityIndicator />;
+    if (user.isFetching)
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <ActivityIndicator />
+            </View>
+        );
 
     return (
-        <View style={{ flex: 1 }}>
-            <UserBanner bannerImage={user.data?.Viewer?.bannerImage} />
-            <ScrollView
-                style={{ flex: 1 }}
-                refreshControl={<RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />}
-            >
+        <FadeHeaderProvider
+            title={user?.data?.Viewer?.name}
+            disableBack
+            addFriendIcon
+            onAddFriend={() => setShowAddFriend(true)}
+            RefreshControl={<RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />}
+            // shareLink={shareLink}
+            loading={user?.isFetching}
+        >
+            <View style={{ flex: 1 }}>
+                <UserBanner bannerImage={user.data?.Viewer?.bannerImage} />
                 <LinearGradient
                     locations={[0, 0.2]}
                     colors={['rgba(0,0,0,0.2)', colors.background]}
@@ -51,7 +74,10 @@ const AuthUserScreen = ({ userID }: { userID: number }) => {
                     <StatOverview
                         anime={user.data?.Viewer?.statistics?.anime}
                         manga={user.data?.Viewer?.statistics?.manga}
+                        activity={user.data?.Viewer?.stats?.activityHistory}
+                        navToStats={navToStats}
                     />
+                    {/* <ActivityOverview data={activity.data?.Page?.activities} /> */}
                     <FavOverview favorites={user.data?.Viewer?.favourites} />
                     <FollowRow
                         title="Following"
@@ -64,8 +90,11 @@ const AuthUserScreen = ({ userID }: { userID: number }) => {
                         isLoading={followers.isLoading}
                     />
                 </LinearGradient>
-            </ScrollView>
-        </View>
+            </View>
+            <Portal>
+                <AddFriendDialog visible={showAddFriend} onDismiss={dismissAddFriend} />
+            </Portal>
+        </FadeHeaderProvider>
     );
 };
 
