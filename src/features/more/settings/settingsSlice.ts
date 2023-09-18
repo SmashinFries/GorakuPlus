602bb011c5ad80c89444feea734fcb6e45680fa3
Smashinfries <jus10luka@gmail.com>
2023-09-17 20:06:28 -0500

    add settings

--- src/features/more/settings/settingsSlice.ts
+++ src/features/more/settings/settingsSlice.ts
@@ -4,13 +4,19 @@ import { Appearance } from 'react-native';
 import { StackAnimationTypes } from 'react-native-screens';
 import { ExploreTabsProps } from '../../../navigation/types';
 import Constants, { ExecutionEnvironment } from 'expo-constants';
+import { InitialState } from '@react-navigation/native';
+import { MediaListStatus } from '../../../app/services/anilist/generated-anilist';
 
 // Define a type for the slice state
-export interface SettingsState {
+export type SettingsState = {
     navAnimation?: StackAnimationTypes;
     btmTabLabels?: boolean;
     exploreTabs?: (keyof ExploreTabsProps)[];
     exploreTabOrder?: (keyof ExploreTabsProps)[];
+    listATabOrder?: (MediaListStatus | string)[];
+    listMTabOrder?: (MediaListStatus | string)[];
+    animeCustomLists?: string[];
+    mangaCustomLists?: string[];
     mediaLanguage?: 'romaji' | 'english' | 'native';
     defaultDescription?: 'ani' | 'mal';
     showNSFW?: boolean;
@@ -18,14 +24,40 @@ export interface SettingsState {
         red: number;
         yellow: number;
     };
-}
+    scoreGlow?: boolean;
+    scoreHealthBar?: boolean;
+    scoreNumber?: boolean;
+    showItemListStatus?: boolean;
+    defaultScore?: 'average' | 'mean';
+    defaultGenreLayout?: 'list' | 'row';
+    defaultTagLayout?: 'list' | 'row';
+    globalTagExclude?: string[];
+};
 
 // Define the initial state using that type
 const initialState: SettingsState = {
-    navAnimation: 'fade_from_bottom',
+    navAnimation: 'slide_from_bottom',
     btmTabLabels: true,
     exploreTabs: ['anime', 'manga', 'manhwa', 'novels'],
     exploreTabOrder: ['anime', 'manga', 'manhwa', 'novels'],
+    listATabOrder: [
+        MediaListStatus.Current,
+        MediaListStatus.Planning,
+        MediaListStatus.Completed,
+        MediaListStatus.Repeating,
+        MediaListStatus.Paused,
+        MediaListStatus.Dropped,
+    ],
+    listMTabOrder: [
+        MediaListStatus.Current,
+        MediaListStatus.Planning,
+        MediaListStatus.Completed,
+        MediaListStatus.Repeating,
+        MediaListStatus.Paused,
+        MediaListStatus.Dropped,
+    ],
+    animeCustomLists: [],
+    mangaCustomLists: [],
     mediaLanguage: 'english',
     defaultDescription: 'mal',
     showNSFW: Constants.executionEnvironment === ExecutionEnvironment.StoreClient ? false : true,
@@ -33,6 +65,26 @@ const initialState: SettingsState = {
         red: 64,
         yellow: 74,
     },
+    scoreHealthBar: true,
+    scoreGlow: false,
+    scoreNumber: false,
+    showItemListStatus: true,
+    defaultScore: 'average',
+    defaultGenreLayout: 'row',
+    defaultTagLayout: 'list',
+    globalTagExclude: [],
+};
+
+type settingsActions = {
+    entryType: keyof SettingsState;
+    value: SettingsState[keyof SettingsState];
+};
+
+export type mediaCardAppearanceActions = {
+    scoreHealthBar: boolean;
+    scoreGlow: boolean;
+    scoreNumber: boolean;
+    showItemListStatus: boolean;
 };
 
 export const settingsSlice = createSlice({
@@ -40,21 +92,30 @@ export const settingsSlice = createSlice({
     // `createSlice` will infer the state type from the `initialState` argument
     initialState,
     reducers: {
-        setSettings: (state, action: PayloadAction<SettingsState>) => {
-            state.navAnimation = action.payload.navAnimation ?? state.navAnimation;
-            state.btmTabLabels = action.payload.btmTabLabels ?? state.btmTabLabels;
-            state.exploreTabs = action.payload.exploreTabs ?? state.exploreTabs;
-            state.exploreTabOrder = action.payload.exploreTabOrder ?? state.exploreTabOrder;
-            state.mediaLanguage = action.payload.mediaLanguage ?? state.mediaLanguage;
-            state.defaultDescription =
-                action.payload.defaultDescription ?? state.defaultDescription;
-            state.showNSFW = action.payload.showNSFW ?? state.showNSFW;
-            state.scoreColors = action.payload.scoreColors ?? state.scoreColors;
+        setSettings: (state, action: PayloadAction<settingsActions>) => {
+            state[action.payload.entryType] = action.payload.value;
+            // state.navAnimation = action.payload.navAnimation ?? state.navAnimation;
+            // state.btmTabLabels = action.payload.btmTabLabels ?? state.btmTabLabels;
+            // state.exploreTabs = action.payload.exploreTabs ?? state.exploreTabs;
+            // state.exploreTabOrder = action.payload.exploreTabOrder ?? state.exploreTabOrder;
+            // state.mediaLanguage = action.payload.mediaLanguage ?? state.mediaLanguage;
+            // state.defaultDescription =
+            //     action.payload.defaultDescription ?? state.defaultDescription;
+            // state.showNSFW = action.payload.showNSFW ?? state.showNSFW;
+            // state.scoreColors = action.payload.scoreColors ?? state.scoreColors;
+            // state.scoreBorders = action.payload.scoreBorders ?? state.scoreBorders;
+        },
+        setMediaCardAppearance: (state, action: PayloadAction<mediaCardAppearanceActions>) => {
+            state.scoreGlow = action.payload.scoreGlow ?? state.scoreGlow;
+            state.scoreHealthBar = action.payload.scoreHealthBar ?? state.scoreHealthBar;
+            state.scoreNumber = action.payload.scoreNumber ?? state.scoreNumber;
+            state.showItemListStatus =
+                action.payload.showItemListStatus ?? state.showItemListStatus;
         },
     },
 });
 
-export const { setSettings } = settingsSlice.actions;
+export const { setSettings, setMediaCardAppearance } = settingsSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
 // export const selectCount = (state: RootState) => state.persistedReducer.;
