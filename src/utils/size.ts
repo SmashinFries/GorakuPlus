acfdea07fb8f3bb5d3fb63afe25b742797f92366
Smashinfries <jus10luka@gmail.com>
2023-09-17 20:21:52 -0500

    add size tools

--- src/utils/size.ts
+++ src/utils/size.ts
@@ -1,15 +1,53 @@
-import { useState, useEffect } from 'react';
-import { useWindowDimensions } from 'react-native';
+import { useState, useEffect, useMemo } from 'react';
+import { Dimensions, useWindowDimensions } from 'react-native';
 
 export const useColumns = (itemWidth: number) => {
     const { width } = useWindowDimensions();
     const [listKey, setListKey] = useState(1);
 
-    const columns = Math.floor(width / itemWidth);
+    const columns = useMemo(() => Math.floor(width / itemWidth), [itemWidth, width]);
 
     useEffect(() => {
         setListKey((prev) => prev + 1);
     }, [width]);
 
     return { columns, listKey };
+};
+
+const gcd = (a: number, b: number) => {
+    return b == 0 ? a : gcd(b, a % b);
+};
+
+export const getAspectRatio = (width: number, height: number) => {
+    const ratio = gcd(width, height);
+    return `${width / ratio}:${height / ratio}`;
+};
+
+export const getAR = (value: number, limit = 50) => {
+    let lower = [0, 1];
+    let upper = [1, 0];
+
+    while (true) {
+        const mediant = [lower[0] + upper[0], lower[1] + upper[1]];
+
+        if (value * mediant[1] > mediant[0]) {
+            if (limit < mediant[1]) {
+                return upper;
+            }
+            lower = mediant;
+        } else if (value * mediant[1] == mediant[0]) {
+            if (limit >= mediant[1]) {
+                return mediant;
+            }
+            if (lower[1] < upper[1]) {
+                return lower;
+            }
+            return upper;
+        } else {
+            if (limit < mediant[1]) {
+                return lower;
+            }
+            upper = mediant;
+        }
+    }
 };
