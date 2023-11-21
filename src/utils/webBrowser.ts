b8bb31b40d51191750a2f4aa415e07300e8dcbed
Smashinfries <jus10luka@gmail.com>
2023-11-20 18:20:45 -0600

    block non urls from opening in browser

--- src/utils/webBrowser.ts
+++ src/utils/webBrowser.ts
@@ -1,5 +1,7 @@
 import * as WebBrowser from 'expo-web-browser';
 
 export const openWebBrowser = async (link: string) => {
+    if (!link || !link?.includes('https')) return;
+
     await WebBrowser.openBrowserAsync(link);
 };
