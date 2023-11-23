0c9ac25c8b653885c4c48e2c4acf689f62b42298
Smashinfries <jus10luka@gmail.com>
2023-11-22 19:50:11 -0600

    add about button component

---
+++ src/components/more/settings/about/buttons.tsx
@@ -0,0 +1,42 @@
+import { copyToClipboard } from '@/utils';
+import { openWebBrowser } from '@/utils/webBrowser';
+import { View } from 'react-native';
+import { IconButton, Text } from 'react-native-paper';
+import { IconSource } from 'react-native-paper/lib/typescript/components/Icon';
+
+type LinkButtonProps = {
+    url: string;
+    icon: IconSource;
+    label?: string;
+    iconColor?: string;
+    transparentBg?: boolean;
+    bgColor?: string;
+    size?: number;
+};
+export const LinkButton = ({
+    url,
+    icon,
+    label,
+    iconColor,
+    transparentBg,
+    bgColor,
+    size,
+}: LinkButtonProps) => {
+    return (
+        <View style={{ alignItems: 'center', marginHorizontal: 10, marginBottom: 20 }}>
+            <IconButton
+                mode="contained"
+                size={size ?? 36}
+                iconColor={iconColor ?? undefined}
+                icon={icon}
+                onPress={() => openWebBrowser(url)}
+                onLongPress={() => copyToClipboard(url)}
+                style={[
+                    transparentBg && { backgroundColor: 'transparent' },
+                    bgColor && { backgroundColor: bgColor },
+                ]}
+            />
+            {label ? <Text numberOfLines={2}>{label}</Text> : null}
+        </View>
+    );
+};
