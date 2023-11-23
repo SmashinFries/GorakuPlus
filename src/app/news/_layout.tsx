0dda9d382f161e34b55630945fab04d905f3f313
Smashinfries <jus10luka@gmail.com>
2023-11-23 12:06:18 -0600

    add news header

--- src/app/news/_layout.tsx
+++ src/app/news/_layout.tsx
@@ -1,14 +1,15 @@
 import PaperHeader from '@/components/headers';
 import { useAppSelector } from '@/store/hooks';
-import { Slot } from 'expo-router';
+import { Stack } from 'expo-router';
 
 const NewsLayout = () => {
     const { navAnimation } = useAppSelector((state) => state.persistedSettings);
     return (
-        <Slot
+        <Stack
             screenOptions={{
                 animation: navAnimation,
                 header: (props) => <PaperHeader {...props} />,
+                title: 'News',
             }}
         />
     );
