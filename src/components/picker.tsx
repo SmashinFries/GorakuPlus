d65aa8322da1460e45fa674eb3610877b30a5431
Smashinfries <jus10luka@gmail.com>
2023-11-28 16:50:50 -0600

    fix wheel picker

--- src/components/picker.tsx
+++ src/components/picker.tsx
@@ -1,3 +1,6 @@
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
+import { BottomSheetModalMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
+import { useMemo } from 'react';
 import { useTheme } from 'react-native-paper';
 import WheelPicker from 'react-native-wheely';
 
@@ -5,13 +8,13 @@ const hours = Array.from(Array(25).keys())
     .map((i) => `${i}`)
     .slice(1);
 
-const scores = Array.from(Array(101).keys())
-    .map((i) => `${i}`)
-    .slice(1);
+const scores = Array.from(Array(101).keys()).map((i) => `${i}`);
 
-export type NumberPickerMode = 'hours' | 'scores' | null;
+const unknownChapterTotal = Array.from(Array(2001).keys()).map((i) => `${i}`);
 
-type NumberPickerProps = {
+export type NumberPickerMode = 'hours' | 'scores' | 'unknown_chapters' | null;
+
+export type NumberPickerProps = {
     defaultValue?: number;
     mode?: NumberPickerMode;
     options?: string[] | null;
@@ -24,15 +27,30 @@ export const NumberPicker = ({
     onChange,
 }: NumberPickerProps) => {
     const { colors } = useTheme();
+    const currentOptions = options
+        ? options
+        : mode === 'hours'
+        ? hours
+        : mode === 'scores'
+        ? scores
+        : unknownChapterTotal;
 
     return (
         <WheelPicker
-            selectedIndex={defaultValue}
+            selectedIndex={defaultValue ?? 0}
             selectedIndicatorStyle={{ backgroundColor: colors.primary, opacity: 0.2 }}
             itemTextStyle={{ color: colors.onSecondaryContainer, fontSize: 20 }}
             // scaleFunction={(x) => x * 0.3}
-            options={options ? options : mode === 'hours' ? hours : scores}
-            onChange={(index) => onChange(Number(hours[index]))}
+            options={
+                options
+                    ? options
+                    : mode === 'hours'
+                    ? hours
+                    : mode === 'unknown_chapters'
+                    ? unknownChapterTotal
+                    : scores
+            }
+            onChange={(index) => onChange(parseInt(currentOptions[index]))}
         />
     );
 };
