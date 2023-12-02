0dc44a493cca566cefb53cefcc8e0151c056426a
Smashinfries <jus10luka@gmail.com>
2023-12-02 00:46:11 -0600

    allow http

--- src/utils/webBrowser.ts
+++ src/utils/webBrowser.ts
@@ -1,7 +1,7 @@
 import * as WebBrowser from 'expo-web-browser';
 
 export const openWebBrowser = async (link: string) => {
-    if (!link || !link?.includes('https')) return;
+    if (!link || !link?.includes('http')) return;
 
     await WebBrowser.openBrowserAsync(link);
 };
