d0951b917a7c84c5779d8a2cc1e98a37423dd2bd
Smashinfries <jus10luka@gmail.com>
2023-12-17 01:07:16 -0600

    add notification context format func

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,3 +1,7 @@
+import {
+    GetNotificationsQuery,
+    UserTitleLanguage,
+} from '@/store/services/anilist/generated-anilist';
 import * as Clipboard from 'expo-clipboard';
 import * as Haptics from 'expo-haptics';
 
@@ -13,4 +17,44 @@ export const countryFlags = {
     KR: '🇰🇷',
     CN: '🇨🇳',
     TW: '🇹🇼',
+};
+
+export const getNotificationMessage = (
+    notif: GetNotificationsQuery['Page']['notifications'][0],
+    language: 'english' | 'native' | 'romaji',
+) => {
+    switch (notif.__typename) {
+        case 'ActivityLikeNotification':
+            return `${notif.user.name} liked your activity`;
+        case 'ActivityMentionNotification':
+            return `${notif.user.name} mentioned you in an activity`;
+        case 'ActivityMessageNotification':
+            return `${notif.user.name} commented on your activity`;
+        case 'ActivityReplyNotification':
+            return `${notif.user.name} replied to your activity`;
+        case 'AiringNotification':
+            return `${notif.contexts[0]}${notif.episode}${notif.contexts[2]}`;
+        case 'FollowingNotification':
+            return `${notif.user.name} started following you`;
+        case 'MediaDataChangeNotification':
+            return notif.reason
+                ? `${notif.media.title[language] ?? notif.media.title.romaji}${notif.context}<br/>${
+                      notif.reason
+                  }`
+                : `${notif.media.title[language] ?? notif.media.title.romaji}${notif.context}`;
+        case 'MediaDeletionNotification':
+            return notif.reason
+                ? `${notif.deletedMediaTitle}${notif.context}<br/>${notif.reason}`
+                : `${notif.deletedMediaTitle}${notif.context}`;
+        case 'MediaMergeNotification':
+            return notif.reason
+                ? `${notif.deletedMediaTitles} merged with ${
+                      notif.media.title[language] ?? notif.media.title.romaji
+                  }<br/>${notif.reason}`
+                : `${notif.deletedMediaTitles} merged with ${
+                      notif.media.title[language] ?? notif.media.title.romaji
+                  }`;
+        case 'RelatedMediaAdditionNotification':
+            return `${notif.context.trim()}`;
+    }
 };
