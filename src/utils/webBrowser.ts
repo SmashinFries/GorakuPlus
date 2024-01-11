76eb837c74dfc98cd86b585461bad0e87acd64f3
Smashinfries <jus10luka@gmail.com>
2024-01-10 18:48:09 -0600

    Refactor openWebBrowser function to support opening links in the device's default browser

--- src/utils/webBrowser.ts
+++ src/utils/webBrowser.ts
@@ -1,7 +1,12 @@
 import * as WebBrowser from 'expo-web-browser';
+import { Linking } from 'react-native';
 
-export const openWebBrowser = async (link: string) => {
+export const openWebBrowser = async (link: string, openInBrowser?: boolean) => {
     if (!link || !link?.includes('http')) return;
 
-    await WebBrowser.openBrowserAsync(link);
+    if (openInBrowser) {
+        await Linking.openURL(link);
+    } else {
+        await WebBrowser.openBrowserAsync(link);
+    }
 };
