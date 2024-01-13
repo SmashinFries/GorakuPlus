4039795a61a9a00224de7dd1cfae7aefd60a6bbc
smashinfries <smashinfries1@gmail.com>
2024-01-13 02:50:31 -0600

    Update gradient locations in MediaBanner component

--- src/components/media/banner.tsx
+++ src/components/media/banner.tsx
@@ -42,7 +42,7 @@ export const MediaBanner = ({ url, style, allowMotion }: Props) => {
 
                 <LinearGradient
                     style={[styles.container]}
-                    locations={[0, 0.9]}
+                    locations={[0, 0.85]}
                     colors={['rgba(0,0,0,.2)', colors.background]}
                 />
             </Animated.View>
