00dfa4d4828df176b0f6a7b121d3728d2ef23d41
Smashinfries <jus10luka@gmail.com>
2024-01-15 16:11:01 -0600

    Add reviews and review info layout components

---
+++ src/app/reviews/_layout.tsx
@@ -0,0 +1,23 @@
+import PaperHeader from '@/components/headers';
+import AnimatedStack from '@/components/stack';
+import { Stack } from 'expo-router';
+
+const ReviewsLayout = () => {
+    return (
+        <AnimatedStack
+            screenOptions={{
+                header: (props) => <PaperHeader {...props} />,
+                headerTransparent: false,
+            }}
+        >
+            <Stack.Screen
+                name="[mediaId]"
+                options={{ title: 'Reviews' }}
+                getId={(params) => params.params?.mediaId}
+            />
+            <Stack.Screen name="info" options={{ title: '', headerShown: false }} />
+        </AnimatedStack>
+    );
+};
+
+export default ReviewsLayout;
