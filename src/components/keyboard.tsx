96f51d38e0eb5205b9e4540fb82b0bcef431d54b
Smashinfries <jus10luka@gmail.com>
2024-01-15 16:10:29 -0600

    Add KeyboardSpacerView component

---
+++ src/components/keyboard.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { View } from 'react-native';
+import { Keyboard, KeyboardEvent } from 'react-native';
+
+export const KeyboardSpacerView = (props) => {
+    const [height, setHeight] = useState<number>(0);
+
+    useEffect(() => {
+        // let event1 = Keyboard.addListener('keyboardWillShow', keyboardWillShow);
+        const event2 = Keyboard.addListener('keyboardDidShow', keyboardWillShow);
+        const event3 = Keyboard.addListener('keyboardWillHide', keyboardWillHide);
+
+        return () => {
+            event2.remove();
+            event3.remove();
+        };
+    }, [false]);
+
+    const keyboardWillShow = (event: KeyboardEvent) => {
+        setHeight(event.endCoordinates.height + 80);
+    };
+
+    const keyboardWillHide = (event: KeyboardEvent) => {
+        setHeight(0);
+    };
+
+    return (
+        <View
+            style={[
+                styles.container,
+                {
+                    height: height,
+                },
+            ]}
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {},
+});
