aea4f2627a8a9390a0e08d6866101ada5722eae2
Smashinfries <jus10luka@gmail.com>
2024-02-19 23:20:30 -0600

    add ts error note

--- src/components/dialogs.tsx
+++ src/components/dialogs.tsx
@@ -101,6 +101,7 @@ export const BarcodeScanDialog = ({ visible, onNav, onDismiss }: BarcodeScanDial
                                 style={{ color: colors.primary }}
                             >
                                 {isbn}{' '}
+                                {/* @ts-ignore | Icon still appears.? */}
                                 <MaterialCommunityIcon name="content-copy" color={colors.primary} />
                             </Text>{' '}
                         </Text>
