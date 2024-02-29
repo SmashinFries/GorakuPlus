0dbf4cc5c77caf3368076e9649ea5567ca6a1a95
Smashinfries <jus10luka@gmail.com>
2024-02-28 18:48:51 -0600

    Refactor useCalendar hook to organize weekly anime data

--- src/hooks/calendar/useCalendar.ts
+++ src/hooks/calendar/useCalendar.ts
@@ -5,14 +5,24 @@ import {
 } from '@/store/services/anilist/generated-anilist';
 import { getWeekStartEnd } from '@/utils';
 
+type WeekData = {
+    sunday: WeeklyAnimeQuery['Page']['airingSchedules'];
+    monday: WeeklyAnimeQuery['Page']['airingSchedules'];
+    tuesday: WeeklyAnimeQuery['Page']['airingSchedules'];
+    wednesday: WeeklyAnimeQuery['Page']['airingSchedules'];
+    thursday: WeeklyAnimeQuery['Page']['airingSchedules'];
+    friday: WeeklyAnimeQuery['Page']['airingSchedules'];
+    saturday: WeeklyAnimeQuery['Page']['airingSchedules'];
+}
+
 export const useCalendar = () => {
     const { start, end } = getWeekStartEnd();
-    const [getWeeklyAnime] = useLazyWeeklyAnimeQuery();
-    const [data, setData] = useState<WeeklyAnimeQuery['Page']['airingSchedules']>([]);
+    const [getWeeklyAnime, weeklyAnimeData] = useLazyWeeklyAnimeQuery();
+    const [data, setData] = useState<WeekData>();
     const [loading, setLoading] = useState(true);
 
     const getAllResults = async () => {
-        const tempData = [];
+        const tempData:WeeklyAnimeQuery['Page']['airingSchedules'] = [];
         let page = 1;
         let fetchMore = true;
         while (fetchMore === true) {
@@ -34,12 +44,53 @@ export const useCalendar = () => {
                 fetchMore = false;
             }
         }
-        setData((prev) => tempData);
+        const week_days:WeekData = {
+            sunday: tempData.filter(
+                (ep) =>
+                    ep.airingAt > start && ep.airingAt < start + 86400,
+            ),
+            monday: tempData.filter(
+                (ep) =>
+                    ep.airingAt > start + 86400 &&
+                    ep.airingAt < start + 86400 * 2,
+            ),
+            tuesday: tempData.filter(
+                (ep) =>
+                    ep.airingAt > start + 86400 * 2 &&
+                    ep.airingAt < start + 86400 * 3,
+            ),
+            wednesday: tempData.filter(
+                (ep) =>
+                    ep.airingAt > start + 86400 * 3 &&
+                    ep.airingAt < start + 86400 * 4,
+            ),
+            thursday: tempData.filter(
+                (ep) =>
+                    ep.airingAt > start + 86400 * 4 &&
+                    ep.airingAt < start + 86400 * 5,
+            ),
+            friday: tempData.filter(
+                (ep) =>
+                    ep.airingAt > start + 86400 * 5 &&
+                    ep.airingAt < start + 86400 * 6,
+            ),
+            saturday: tempData.filter(
+                (ep) =>
+                    ep.airingAt > start + 86400 * 6 && ep.airingAt < end,
+            )
+        }
+        setData(week_days);
         setLoading(false);
     };
 
+    // useEffect(() => {
+    //     if (weeklyAnimeData.isError) {
+    //         sendErrorMessage(`${ + ' - '}${weeklyAnimeData.error?.message}`);
+    //     }
+    // },[weeklyAnimeData])
+
     useEffect(() => {
-        data.length < 1 && getAllResults();
+        getAllResults();
     }, []);
 
     return { data, week: { start, end }, loading, refetch: getAllResults };
