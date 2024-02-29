8708ba73574c00ab90873f038f8d4093b8cf1f0c
Smashinfries <jus10luka@gmail.com>
2024-02-29 14:40:29 -0600

    Add ListSortOptions and ListSortOptionsType to anilist.ts

--- src/types/anilist.ts
+++ src/types/anilist.ts
@@ -1,4 +1,4 @@
-import { ExploreMediaQueryVariables } from '@/store/services/anilist/generated-anilist';
+import { ExploreMediaQueryVariables, MediaListSort } from '@/store/services/anilist/generated-anilist';
 import { commonSorts } from '@/constants/anilist';
 
 export type FilterOptions = Partial<ExploreMediaQueryVariables>;
@@ -6,3 +6,12 @@ export type FilterOptions = Partial<ExploreMediaQueryVariables>;
 export type UpdateFilter = (filter: ExploreMediaQueryVariables) => void;
 
 export type SortModes = keyof typeof commonSorts;
+
+export const ListSortOptions = {
+    ...MediaListSort,
+    AverageScoreDesc: 'AverageScore_DESC',
+    AverageScore: 'AverageScore',
+    MeanScoreDesc: 'MeanScore_DESC',
+    MeanScore: 'MeanScore',
+};
+export type ListSortOptionsType = (typeof ListSortOptions)[keyof typeof ListSortOptions];
