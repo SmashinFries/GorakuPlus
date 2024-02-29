e9ade5b6bc4b1ea3341f95261af11beb21c4bd6b
Smashinfries <jus10luka@gmail.com>
2024-02-28 18:45:41 -0600

    Add blurhash theme support to app themes

--- src/store/theme/theme.ts
+++ src/store/theme/theme.ts
@@ -7,7 +7,7 @@ import {
     DarkTheme as NavigationDarkTheme,
     DefaultTheme as NavigationDefaultTheme,
 } from '@react-navigation/native';
-import { MD3DarkTheme, MD3LightTheme, adaptNavigationTheme } from 'react-native-paper';
+import { MD3DarkTheme, MD3LightTheme, adaptNavigationTheme, useTheme } from 'react-native-paper';
 import { KawaiiDark, KawaiiLight } from './m3/kawaiiTheme';
 import { PunpunDark, PunpunLight } from './m3/punpunTheme';
 import { MionDark, MionLight } from './m3/mionTheme';
@@ -19,12 +19,32 @@ const { LightTheme, DarkTheme } = adaptNavigationTheme({
     reactNavigationDark: NavigationDarkTheme,
 });
 
+const blurHashTheme = {
+    light: {
+        mi_chan: 'BvOqG-SK^-FYWAwM',
+        hinata: 'LqQSlCRk_Mt7tht6xujYM|WBspa$',
+        kawaii: 'LNRnE4xc-@Z~Vat8o|WByEn6o_tj',
+        punpun: 'LMN^e:t7~qay~qIU-;?b-;WBofof',
+        aqua: 'LcO4F~Vs~pkq-;-:s:E1-oR*M|R*',
+        berserk: 'LeN5[8og|xr@^R$+J6J6=fS$NFsV'
+    },
+    dark: {
+        mi_chan: 'B35E.^z]5Q?@m:NZ',
+        hinata: 'L59%G6kC00RP0Jay-:kD00Rj~ntR',
+        kawaii: 'L6JP.c[f00LW000d~W=h0J~p]?Hu',
+        punpun: 'LMN^e:t7~qay~qIU-;?b-;WBofof',
+        aqua: 'L8DA$V9jB=-f00VBZ2JXX=ox-:Rl',
+        berserk: 'LeN5[8og|xr@^R$+J6J6=fS$NFsV'
+    }
+}
+
 const HinataLightTheme = {
     ...MD3LightTheme,
     ...LightTheme,
     colors: {
         ...MD3LightTheme.colors,
         ...LightTheme.colors,
+        blurhash: blurHashTheme.light.hinata,
     },
 };
 const HinataDarkTheme = {
@@ -33,6 +53,7 @@ const HinataDarkTheme = {
     colors: {
         ...MD3DarkTheme.colors,
         ...DarkTheme.colors,
+        blurhash: blurHashTheme.dark.hinata,
     },
 };
 
@@ -41,6 +62,7 @@ const KawaiiLightTheme = {
     colors: {
         ...HinataLightTheme.colors,
         ...KawaiiLight,
+        blurhash: blurHashTheme.light.kawaii,
     },
 };
 
@@ -49,6 +71,7 @@ const KawaiiDarkTheme = {
     colors: {
         ...HinataDarkTheme.colors,
         ...KawaiiDark,
+        blurhash: blurHashTheme.dark.kawaii,
     },
 };
 
@@ -57,6 +80,7 @@ const PunpunLightTheme = {
     colors: {
         ...HinataLightTheme.colors,
         ...PunpunLight,
+        blurhash: blurHashTheme.light.punpun,
     },
 };
 
@@ -65,6 +89,7 @@ const PunpunDarkTheme = {
     colors: {
         ...HinataDarkTheme.colors,
         ...PunpunDark,
+        blurhash: blurHashTheme.dark.punpun,
     },
 };
 
@@ -73,6 +98,7 @@ const MionLightTheme = {
     colors: {
         ...HinataLightTheme.colors,
         ...MionLight,
+        blurhash: blurHashTheme.light.mi_chan,
     },
 };
 
@@ -81,6 +107,7 @@ const MionDarkTheme = {
     colors: {
         ...HinataDarkTheme.colors,
         ...MionDark,
+        blurhash: blurHashTheme.dark.mi_chan,
     },
 };
 
@@ -89,6 +116,7 @@ const AquaLightTheme = {
     colors: {
         ...HinataLightTheme.colors,
         ...AquaLight,
+        blurhash: blurHashTheme.light.aqua,
     },
 };
 
@@ -97,6 +125,7 @@ const AquaDarkTheme = {
     colors: {
         ...HinataLightTheme.colors,
         ...AquaDark,
+        blurhash: blurHashTheme.dark.aqua,
     },
 };
 
@@ -105,14 +134,16 @@ const BerserkLightTheme = {
     colors: {
         ...HinataLightTheme.colors,
         ...BerserkLight,
+        blurhash: blurHashTheme.light.berserk,
     },
 };
 
 const BerserkDarkTheme = {
     ...HinataDarkTheme,
     colors: {
-        ...HinataLightTheme.colors,
+        ...HinataDarkTheme.colors,
         ...BerserkDark,
+        blurhash: blurHashTheme.dark.berserk,
     },
 };
 
@@ -137,4 +168,8 @@ const availableThemes = {
     dark: darkThemes,
 };
 
-export { availableThemes, themeOptions };
+type AppTheme = typeof HinataLightTheme;
+
+const useAppTheme = () => useTheme<AppTheme>();
+
+export { availableThemes, themeOptions, useAppTheme, AppTheme };
