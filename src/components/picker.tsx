7b698099b60a7e359cf1f696415f75cd8a7be959
Smashinfries <jus10luka@gmail.com>
2024-02-29 20:03:58 -0600

    Refactor NumberPicker component to support custom score formats

--- src/components/picker.tsx
+++ src/components/picker.tsx
@@ -1,6 +1,8 @@
+import { ScoreFormat } from '@/store/services/anilist/generated-anilist';
+import { scoreStringToNumber, scoreToIndex, scoreValues } from '@/utils/scores';
 import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import { BottomSheetModalMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useTheme } from 'react-native-paper';
 import WheelPicker from 'react-native-wheely';
 
@@ -8,11 +10,11 @@ const hours = Array.from(Array(25).keys())
     .map((i) => `${i}`)
     .slice(1);
 
-const scores = Array.from(Array(101).keys()).map((i) => `${i}`);
+// const scores = Array.from(Array(101).keys()).map((i) => `${i}`);
 
 const unknownChapterTotal = Array.from(Array(2001).keys()).map((i) => `${i}`);
 
-export type NumberPickerMode = 'hours' | 'scores' | 'unknown_chapters' | null;
+export type NumberPickerMode = 'hours' | 'unknown_chapters' | ScoreFormat;
 
 export type NumberPickerProps = {
     defaultValue?: number;
@@ -21,36 +23,46 @@ export type NumberPickerProps = {
     onChange?: (value: number) => void;
 };
 export const NumberPicker = ({
-    defaultValue = 11,
+    defaultValue = 0,
     mode = 'hours',
     options = null,
     onChange,
 }: NumberPickerProps) => {
-    const { colors } = useTheme();
-    const currentOptions = options
+    console.log('FIRST INDEX', defaultValue);
+    const currentOptions: string[] = options
         ? options
         : mode === 'hours'
-        ? hours
-        : mode === 'scores'
-        ? scores
-        : unknownChapterTotal;
+          ? hours
+          : Object.values(ScoreFormat).includes(mode as ScoreFormat)
+            ? scoreValues[mode]
+            : unknownChapterTotal;
+
+    const [index, setIndex] = useState(
+        Object.values(ScoreFormat).includes(mode as ScoreFormat)
+            ? scoreToIndex(defaultValue, mode as ScoreFormat)
+            : defaultValue,
+    );
+    const { colors } = useTheme();
+
+    const onIndexChange = (idx: number) => {
+        setIndex(idx);
+    };
+
+    useEffect(() => {
+        console.log('index:', index);
+        if (Object.values(ScoreFormat).includes(mode as ScoreFormat)) {
+            onChange && onChange(scoreStringToNumber(currentOptions[index], mode as ScoreFormat));
+        }
+    }, [index]);
 
     return (
         <WheelPicker
-            selectedIndex={defaultValue ?? 0}
+            selectedIndex={index > currentOptions.length - 1 ? 0 : index}
             selectedIndicatorStyle={{ backgroundColor: colors.primary, opacity: 0.2 }}
             itemTextStyle={{ color: colors.onSecondaryContainer, fontSize: 20 }}
             // scaleFunction={(x) => x * 0.3}
-            options={
-                options
-                    ? options
-                    : mode === 'hours'
-                    ? hours
-                    : mode === 'unknown_chapters'
-                    ? unknownChapterTotal
-                    : scores
-            }
-            onChange={(index) => onChange(parseInt(currentOptions[index]))}
+            options={currentOptions}
+            onChange={onIndexChange}
         />
     );
 };
