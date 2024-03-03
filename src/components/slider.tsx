d9cb18d8c5821c32d5adf303fa975f5a04bae74e
Smashinfries <jus10luka@gmail.com>
2024-03-03 00:36:36 -0600

    Add GorakuSlider component

---
+++ src/components/slider.tsx
@@ -0,0 +1,41 @@
+import Slider from '@react-native-community/slider';
+import { useState } from 'react';
+import { StyleProp, ViewStyle } from 'react-native';
+import { View } from 'react-native';
+import { NativeViewGestureHandler } from 'react-native-gesture-handler';
+import { Text, useTheme } from 'react-native-paper';
+
+type GorakuSliderProps = {
+	updateValue: (val: number) => void;
+	value: number;
+	minValue?: number;
+	maxValue?: number;
+	step?: number;
+	style?: StyleProp<ViewStyle>;
+};
+export const GorakuSlider = ({
+	updateValue,
+	value,
+	maxValue = 100,
+	minValue = 0,
+	step = 5,
+	style,
+}: GorakuSliderProps) => {
+	const { colors } = useTheme();
+	return (
+		<Slider
+			value={value}
+			onValueChange={(value) => updateValue(value)}
+			// onSlidingComplete={(value) => updateValue(value)}
+			minimumTrackTintColor={colors.primary}
+			maximumTrackTintColor={colors.secondary}
+			step={step}
+			minimumValue={minValue}
+			maximumValue={maxValue}
+			thumbTintColor={colors.primary}
+			lowerLimit={minValue ?? null}
+			upperLimit={maxValue ?? null}
+			style={[style]}
+		/>
+	);
+};
