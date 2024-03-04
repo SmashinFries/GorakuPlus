632c2ee9a18fd6461ba7cad994681d211d30e730
Smashinfries <jus10luka@gmail.com>
2024-03-03 21:52:07 -0600

    Add CharStaffInteractionBar component

---
+++ src/components/characters/interaction.tsx
@@ -0,0 +1,125 @@
+import { useAppTheme } from '@/store/theme/theme';
+import { saveImage } from '@/utils/images';
+import { StyleSheet, useWindowDimensions } from 'react-native';
+import { Share, View } from 'react-native';
+import { ActivityIndicator, Divider, IconButton, Text } from 'react-native-paper';
+import { ActionIcon } from '../media/sections/entry';
+import { useAppSelector } from '@/store/hooks';
+import { openWebBrowser } from '@/utils/webBrowser';
+
+type CharStaffInteractionBarProps = {
+	isFav: boolean;
+	favLoading: boolean;
+	toggleFav: () => void;
+	share_url: string;
+	edit_url: string;
+};
+export const CharStaffInteractionBar = ({
+	isFav,
+	favLoading,
+	edit_url,
+	share_url,
+	toggleFav,
+}: CharStaffInteractionBarProps) => {
+	const { userID } = useAppSelector((state) => state.persistedAniLogin);
+	const { colors } = useAppTheme();
+	const { width } = useWindowDimensions();
+	const containerWidth = width / 3;
+
+	return (
+		<View>
+			<View
+				style={{
+					flexDirection: 'row',
+					marginTop: 15,
+					alignItems: 'flex-start',
+				}}
+			>
+				<View
+					style={{
+						width: containerWidth,
+						borderRadius: 12,
+						alignItems: 'center',
+					}}
+				>
+					<ActionIcon
+						icon={'file-document-edit-outline'}
+						onPress={() => openWebBrowser(edit_url)}
+					>
+						<Text
+							style={{
+								textTransform: 'capitalize',
+								color: colors.onSurfaceVariant,
+								textAlign: 'center',
+							}}
+							variant="labelMedium"
+						>
+							{'Edit'}
+						</Text>
+					</ActionIcon>
+				</View>
+				<View
+					style={{
+						width: containerWidth,
+						borderRadius: 12,
+						alignItems: 'center',
+					}}
+				>
+					<ActionIcon
+						icon={'share-variant-outline'}
+						onPress={() => Share.share({ url: share_url, message: share_url })}
+					>
+						<Text
+							style={{
+								textTransform: 'capitalize',
+								color: colors.onSurfaceVariant,
+								textAlign: 'center',
+							}}
+							variant="labelMedium"
+						>
+							{'Share'}
+						</Text>
+					</ActionIcon>
+				</View>
+				<View style={{ width: containerWidth, borderRadius: 12, alignItems: 'center' }}>
+					{favLoading ? (
+						<ActivityIndicator
+							animating
+							size={'small'}
+							style={{ transform: [{ scale: 0.9 }] }}
+						/>
+					) : (
+						<ActionIcon onPress={() => toggleFav()}>
+							<IconButton
+								icon={isFav ? 'heart' : 'heart-outline'}
+								iconColor={isFav ? colors.primary : null}
+								disabled={userID ? false : true}
+								size={24}
+							/>
+							<Text
+								style={{
+									textTransform: 'capitalize',
+									color: isFav ? colors.primary : colors.onSurfaceVariant,
+								}}
+								variant="labelMedium"
+							>
+								{isFav ? 'Favorited' : 'Favorite'}
+							</Text>
+						</ActionIcon>
+					)}
+				</View>
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		marginTop: 20,
+	},
+	iconsContainer: {
+		flexDirection: 'row',
+		marginTop: 5,
+		justifyContent: 'space-evenly',
+	},
+});
