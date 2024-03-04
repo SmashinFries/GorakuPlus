244240fb6cb6f4af23462ed0ca4a7b6c9051df5a
Smashinfries <jus10luka@gmail.com>
2024-03-03 21:51:05 -0600

    Refactor useTTS to accept TTSVoice object

--- src/hooks/useTTS.ts
+++ src/hooks/useTTS.ts
@@ -1,13 +1,18 @@
+import { TTSVoice } from '@/store/slices/ttsSlice';
 import * as Speech from 'expo-speech';
 
 const useTTS = () => {
-	const speak = async (text: string, config?: Speech.SpeechOptions) => {
-		if (!config?.voice) return;
+	const speak = async (text: string, voice: TTSVoice) => {
+		if (!voice?.voice) return;
 		const isSpeaking = await Speech.isSpeakingAsync();
 		if (isSpeaking) {
 			await Speech.stop();
 		} else {
-			Speech.speak(text, config);
+			Speech.speak(text, {
+				voice: voice.voice.identifier,
+				pitch: voice.pitch,
+				rate: voice.rate,
+			});
 		}
 	};
 
