102ec3e7c8e1eaf1497a7308f9a710cac2ef5845
Smashinfries <jus10luka@gmail.com>
2024-04-21 17:09:15 -0500

    add scrollToTop to music list

--- src/components/music/list.tsx
+++ src/components/music/list.tsx
@@ -1,17 +1,23 @@
 import { FlashList } from '@shopify/flash-list';
 import { Animetheme, MainMusic } from '@/store/services/animethemes/types';
 import { View, useWindowDimensions } from 'react-native';
-import { MusicItem, MusicVideo, MusicVideoYT } from './video';
+import { MusicItem } from './video';
+import { useRef, useState } from 'react';
+import { ScrollToTopButton } from '../buttons';
 
 type MusicVideoListProps = {
 	music: MainMusic;
 };
 export const MusicVideoList = ({ music }: MusicVideoListProps) => {
 	const { width, height } = useWindowDimensions();
+	const [scrollOffset, setScrollOffset] = useState<number>(0);
+	const listRef = useRef<FlashList<Animetheme>>(null);
+
 	if (music?.anime?.length < 1) return null;
 	return (
 		<View style={{ flex: 1, width: width }}>
 			<FlashList
+				ref={listRef}
 				data={music?.anime[0]?.animethemes}
 				renderItem={(props) => (
 					<MusicItem
@@ -24,7 +30,11 @@ export const MusicVideoList = ({ music }: MusicVideoListProps) => {
 				estimatedItemSize={66}
 				numColumns={1}
 				contentContainerStyle={{ paddingVertical: 15 }}
+				onScroll={(e) => setScrollOffset(e.nativeEvent.contentOffset.y)}
 			/>
+			{scrollOffset > 500 && music?.anime[0]?.animethemes?.length > 9 && (
+				<ScrollToTopButton listRef={listRef} />
+			)}
 		</View>
 	);
 };
