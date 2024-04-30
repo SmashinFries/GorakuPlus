56d1830718cb65a14886c00201050b3e3ea03177
Smashinfries <jus10luka@gmail.com>
2024-04-30 17:25:05 -0500

    Add babel-plugin-i18next-extract to Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-        presets: ['babel-preset-expo'],
-        plugins: ['react-native-reanimated/plugin'],
-        env: {
-            production: {
-              plugins: ['react-native-paper/babel'],
-            },
-          },
-    };
+	api.cache(true);
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: ['react-native-reanimated/plugin', ['i18next-extract', { locales: ['en', 'es'] }]],
+		env: {
+			production: {
+				plugins: ['react-native-paper/babel'],
+			},
+		},
+	};
 };
