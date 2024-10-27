17cae21fa0cb7e610220a8f1e90349c9d9e9ec37
Smashinfries <jus10luka@gmail.com>
2024-10-27 15:46:07 -0500

    update eslint prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,45 @@ module.exports = {
 	plugins: ['prettier'],
 	rules: {
 		'prettier/prettier': 'warn',
+		'import/namespace': 'off',
+		'import/resolver': 'off',
+		'react-hooks/exhaustive-deps': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{
+				argsIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				caughtErrorsIgnorePattern: '^_',
+			},
+		],
+		'@typescript-eslint/ban-types': 'off', // deprecated!
+		'@typescript-eslint/no-restricted-types': [
+			'error',
+			{
+				types: {
+					Number: {
+						message: 'Use `number` instead.',
+						fixWith: 'number',
+					},
+					Boolean: {
+						message: 'Use `boolean` instead.',
+						fixWith: 'boolean',
+					},
+					Symbol: {
+						message: 'Use `symbol` instead.',
+						fixWith: 'symbol',
+					},
+					Object: {
+						message: 'Use `object` instead.',
+						fixWith: 'object',
+					},
+					String: {
+						message: 'Use `string` instead.',
+						fixWith: 'string',
+					},
+				},
+			},
+		],
 	},
+	ignorePatterns: ['src/api/*'],
 };
--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    singleQuote: true,
-    trailingComma: 'all',
-    printWidth: 100,
-    tabWidth: 4,
-    useTabs: true,
-    endOfLine: 'crlf',
-};
