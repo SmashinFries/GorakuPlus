98c7fb3203e8ea52dae05c175827a58852790a05
Smashinfries <jus10luka@gmail.com>
2024-12-24 21:50:24 -0600

    feat: add Discord support link to NetworkError component

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -1,7 +1,8 @@
 import { useAppTheme } from '@/store/theme/themes';
+import { openWebBrowser } from '@/utils/webBrowser';
 import { QueryStatus } from '@tanstack/react-query';
 import { StyleSheet, View } from 'react-native';
-import { IconButton, Text, useTheme } from 'react-native-paper';
+import { Button, IconButton, Text, useTheme } from 'react-native-paper';
 
 export const NetworkError = ({
 	status,
@@ -17,7 +18,12 @@ export const NetworkError = ({
 			<View>
 				<IconButton onPress={onRefresh} icon="refresh" size={38} iconColor={colors.error} />
 			</View>
-			<Text>{'Something went wrong :('}</Text>
+			{/* https://discord.gg/eNentGQDMS */}
+			<Text>{'Something went wrong :(\n'}</Text>
+			<Button onPress={() => openWebBrowser('https://discord.gg/gdenWHjXBv', true)}>
+				Check our Discord
+			</Button>
+			{/* <Text style={{ fontStyle: 'italic' }}>AniList API is likely down</Text> */}
 		</View>
 	);
 };
