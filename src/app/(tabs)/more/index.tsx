843d3ab1ceec71b51540f0e1821ffeb6d7fd1abb
Smashinfries <jus10luka@gmail.com>
2025-03-07 02:40:13 -0600

    update nav routes / add update notif

--- src/app/(tabs)/more/index.tsx
+++ src/app/(tabs)/more/index.tsx
@@ -1,18 +1,60 @@
+import { useAppUpdaterStore } from '@/store/appUpdateStore';
 import { useAppTheme } from '@/store/theme/themes';
 import { router } from 'expo-router';
-import { View } from 'react-native';
-import { Divider, List } from 'react-native-paper';
+import { Pressable, View } from 'react-native';
+import { Button, Divider, Icon, List, Surface, Text } from 'react-native-paper';
 
 const MorePage = () => {
 	const { colors } = useAppTheme();
+	const { updateDetails } = useAppUpdaterStore();
 
 	return (
 		<View style={{ flex: 1 }}>
+			{updateDetails && (
+				<>
+					<Divider />
+					<Pressable
+						onPress={() => router.navigate('/(sheets)/appUpdateSheet')}
+						android_ripple={{ color: colors.primary, foreground: true }}
+						style={{ margin: 8, overflow: 'hidden', borderRadius: 8 }}
+					>
+						<View
+							style={{
+								// backgroundColor: colors.elevation.level4,
+								backgroundColor: 'transparent',
+								borderWidth: 1,
+								borderColor: colors.primary,
+								padding: 8,
+
+								borderRadius: 8,
+								paddingVertical: 24,
+							}}
+						>
+							<View
+								style={{
+									flexDirection: 'row',
+									gap: 6,
+									alignItems: 'center',
+								}}
+							>
+								<Icon source={'update'} size={24} color={colors.primary} />
+								<Text variant="titleMedium">New update available!</Text>
+								<View style={{ alignItems: 'flex-end', flex: 1, paddingRight: 6 }}>
+									<Icon size={24} source={'download-outline'} />
+								</View>
+							</View>
+							{/* <View style={{ alignItems: 'flex-end' }}>
+							<Button onPress={() => null}>Update</Button>
+						</View> */}
+						</View>
+					</Pressable>
+				</>
+			)}
 			<Divider />
 			<List.Item
 				title="Weeb Lab"
 				description="A collection of extra features!"
-				onPress={() => router.navigate('/more/weeblab')}
+				onPress={() => router.navigate('/more/weeblabs')}
 				left={(props) => <List.Icon {...props} color={colors.primary} icon="flask" />}
 			/>
 			<Divider />
@@ -32,7 +74,7 @@ const MorePage = () => {
 			/>
 			<List.Item
 				title="AniList Site Stats"
-				onPress={() => router.navigate('/more/sitestats')}
+				onPress={() => router.navigate('/statistics/sitestats')}
 				left={(props) => <List.Icon {...props} color={colors.primary} icon="chart-line" />}
 			/>
 			<List.Item
