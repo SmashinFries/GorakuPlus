7e0a8c4cb2c1c453a0f91aa83ec79b605cc1cae8
Smashinfries <jus10luka@gmail.com>
2025-03-09 16:52:40 -0500

    refactor

--- src/components/barcodeScanner.tsx
+++ src/components/barcodeScanner.tsx
@@ -1,32 +1,47 @@
 import { BasicDialogProps } from '@/types';
-import { BarcodeScanningResult, Camera, CameraView, useCameraPermissions } from 'expo-camera';
+import {
+	BarcodeBounds,
+	BarcodeScanningResult,
+	Camera,
+	CameraView,
+	useCameraPermissions,
+} from 'expo-camera';
 import { useEffect } from 'react';
-import { FadeIn, FadeOut } from 'react-native-reanimated';
+import Animated, {
+	FadeIn,
+	FadeOut,
+	useAnimatedStyle,
+	useSharedValue,
+	withTiming,
+} from 'react-native-reanimated';
 import { AnimViewMem } from './animations';
 import { View } from 'react-native';
 import { Button, Surface } from 'react-native-paper';
 import { useQueryClient } from '@tanstack/react-query';
 import { useIsbnSearch } from '@/api/googlebooks/googlebooks';
-import { SheetManager } from 'react-native-actions-sheet';
 import { MediaType } from '@/api/anilist/__genereated__/gql';
 import { sendErrorMessage } from '@/utils/toast';
 import * as ImagePicker from 'expo-image-picker';
-import { Href, router } from 'expo-router';
+import { RelativePathString, router } from 'expo-router';
 import * as Linking from 'expo-linking';
 
 const BarcodeScanner = ({
 	visible,
 	onDismiss,
-	openScanner,
+	// openScanner,
 }: BasicDialogProps & { openScanner: () => void }) => {
 	const [permission, requestPermission] = useCameraPermissions();
 	const queryClient = useQueryClient();
+	// const codeRectBoundSize = useSharedValue<BarcodeBounds['size']>({ height: 0, width: 0 });
+	// const codeRectBoundOrigin = useSharedValue<BarcodeBounds['origin']>({ x: 0, y: 0 });
 
 	// const { aniData, isLoading, isbn, scanned, handleBarCodeScanned, triggerScan, resetScan } =
 	// 	useBarcode();
 
-	const onBarcodeScan = async ({ data, type }: BarcodeScanningResult) => {
+	const onBarcodeScan = async ({ data, type, bounds }: BarcodeScanningResult) => {
 		// type can be a number if using the scanFromURLAsync method \0_0/
+		// codeRectBoundOrigin.value = withTiming(bounds.origin);
+		// codeRectBoundSize.value = withTiming(bounds.size);
 		if (type === 'ean13' || (type as unknown as number) === 32) {
 			try {
 				const isbn_response = await queryClient.fetchQuery({
@@ -35,14 +50,20 @@ const BarcodeScanner = ({
 				if (isbn_response.data?.items.length > 0) {
 					const title = isbn_response.data?.items[0].volumeInfo.title.split(',')[0];
 					onDismiss();
-					SheetManager.show('MediaSearchSheet', {
-						payload: {
+					router.push({
+						pathname: '/(sheets)/mediaSearchSheet',
+						params: {
 							search: title,
 							type: MediaType.Manga,
 							isbn: data,
-							onRescan: openScanner,
 						},
 					});
+					// openMediaSearchSheet({
+					// 	search: title,
+					// 	type: MediaType.Manga,
+					// 	isbn: data,
+					// 	// onRescan: openScanner,
+					// });
 				} else {
 					onDismiss();
 					sendErrorMessage(isbn_response.statusText ?? 'No Results');
@@ -55,21 +76,29 @@ const BarcodeScanner = ({
 			onDismiss();
 			if (data.includes('anilist')) {
 				const { path } = Linking.parse(data);
-				router.navigate(('/' + path) as Href<string>);
+				router.navigate(('/' + path) as RelativePathString);
 			}
 		}
 	};
 
 	const onImageSelect = async () => {
 		const { assets } = await ImagePicker.launchImageLibraryAsync();
-		const result = await Camera.scanFromURLAsync(assets[0]?.uri, ['qr', 'ean13']);
-		if (result[0]) {
-			await onBarcodeScan(result[0]);
-		} else {
-			onDismiss();
-			sendErrorMessage('No code found');
+		if (assets?.[0]?.uri) {
+			const result = await Camera.scanFromURLAsync(assets?.[0]?.uri, ['qr', 'ean13']);
+			if (result[0]) {
+				await onBarcodeScan(result[0]);
+			} else {
+				onDismiss();
+				sendErrorMessage('No code found');
+			}
 		}
 	};
+
+	// const codeRectStyle = useAnimatedStyle(() => ({
+	// 	left: codeRectBoundOrigin.value.x,
+	// 	top: codeRectBoundOrigin.value.y,
+	// 	...codeRectBoundSize.value,
+	// }));
 
 	useEffect(() => {
 		if (visible) {
@@ -110,6 +139,17 @@ const BarcodeScanner = ({
 					barcodeScannerSettings={{ barcodeTypes: ['qr', 'ean13'] }}
 					onBarcodeScanned={onBarcodeScan}
 				/>
+				{/* <Animated.View
+					style={[
+						codeRectStyle,
+						{
+							borderWidth: 3,
+							borderStyle: 'dashed',
+							borderRadius: 12,
+							borderColor: '#FFF',
+						},
+					]}
+				/> */}
 				<View
 					style={{
 						position: 'absolute',
