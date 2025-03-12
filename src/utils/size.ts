84f6812b6dbe2c88539e2b5597efb2bb216fadba
Smashinfries <jus10luka@gmail.com>
2025-03-11 23:06:09 -0500

    refactor: simplify aspect ratio calculation and improve readability

--- src/utils/size.ts
+++ src/utils/size.ts
@@ -1,37 +1,35 @@
-const gcd = (a: number, b: number) => {
-	return b === 0 ? a : gcd(b, a % b);
-};
-
-export const getAspectRatio = (width: number, height: number) => {
-	const ratio = gcd(width, height);
-	return `${width / ratio}:${height / ratio}`;
-};
-
 export const getAR = (value: number, limit = 50) => {
 	let lower = [0, 1];
 	let upper = [1, 0];
 
 	while (true) {
 		const mediant = [lower[0] + upper[0], lower[1] + upper[1]];
+		const comparison = compareMediant(value, mediant);
 
-		if (value * mediant[1] > mediant[0]) {
-			if (limit < mediant[1]) {
-				return upper;
-			}
+		if (mediant[1] > limit) {
+			return comparison > 0 ? upper : lower;
+		}
+
+		if (comparison === 0) {
+			return chooseFinalRatio(mediant, lower, upper, limit);
+		}
+
+		if (comparison > 0) {
 			lower = mediant;
-		} else if (value * mediant[1] === mediant[0]) {
-			if (limit >= mediant[1]) {
-				return mediant;
-			}
-			if (lower[1] < upper[1]) {
-				return lower;
-			}
-			return upper;
 		} else {
-			if (limit < mediant[1]) {
-				return lower;
-			}
 			upper = mediant;
 		}
 	}
+};
+
+const compareMediant = (value: number, mediant: number[]) => {
+	const comparison = value * mediant[1] - mediant[0];
+	return Math.sign(comparison);
+};
+
+const chooseFinalRatio = (mediant: number[], lower: number[], upper: number[], limit: number) => {
+	if (limit >= mediant[1]) {
+		return mediant;
+	}
+	return lower[1] < upper[1] ? lower : upper;
 };
