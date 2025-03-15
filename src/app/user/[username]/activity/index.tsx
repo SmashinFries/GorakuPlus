cbc1decdd2cfc8198bebe39ec5f8fc0ebf66a961
Smashinfries <jus10luka@gmail.com>
2025-03-14 21:25:57 -0500

    fix: update following activity logic

--- src/app/user/[username]/activity/index.tsx
+++ src/app/user/[username]/activity/index.tsx
@@ -13,40 +13,40 @@ import { GorakuRefreshControl } from '@/components/explore/lists';
 import { GorakuActivityIndicator } from '@/components/loading';
 import { ConfirmActDelDialog } from '@/components/user/dialogs';
 import { useAuthStore } from '@/store/authStore';
-import { FlashList } from '@shopify/flash-list';
 import { useLocalSearchParams } from 'expo-router';
-import { useRef, useState } from 'react';
-import { View } from 'react-native';
-import { Divider, Portal } from 'react-native-paper';
+import { useMemo, useRef, useState } from 'react';
+import { FlatList, View } from 'react-native';
+import { Button, Divider, Portal } from 'react-native-paper';
 import { useShallow } from 'zustand/react/shallow';
 
 const ActivityListPage = () => {
 	const { userId } = useLocalSearchParams<{ userId: string }>();
 	const { viewerId } = useAuthStore(useShallow((state) => ({ viewerId: state.anilist.userID })));
+	const isViewer = useMemo(() => viewerId === Number(userId), [userId, viewerId]);
 	// const mediaLanguage = useSettingsStore(useShallow((state) => state.mediaLanguage));
-	const { data, isFetching, fetchNextPage, hasNextPage, refetch } = useInfiniteUserActivityQuery(
-		{
-			userId: userId === 'user' ? undefined : parseInt(userId),
-			page: 1,
-			perPage: 24,
-			isFollowing: `${viewerId}` !== userId,
-		},
-		{
-			enabled: !!userId,
-			initialPageParam: 1,
-			getNextPageParam(lastPage) {
-				if (lastPage.Page?.pageInfo.hasNextPage) {
-					return {
-						page: lastPage?.Page?.pageInfo.currentPage + 1,
-					};
-				}
+	const { data, isFetching, fetchNextPage, hasNextPage, refetch, isRefetching } =
+		useInfiniteUserActivityQuery(
+			{
+				userId: userId === 'user' || isViewer ? undefined : parseInt(userId),
+				page: 1,
+				perPage: 24,
+				isFollowing: isViewer,
 			},
-		},
-	);
+			{
+				enabled: !!userId,
+				initialPageParam: 1,
+				getNextPageParam(lastPage) {
+					if (lastPage.Page?.pageInfo?.hasNextPage) {
+						return {
+							page: (lastPage?.Page?.pageInfo?.currentPage ?? 0) + 1,
+						};
+					}
+				},
+			},
+		);
 
-	const listRef = useRef<FlashList<ListActivity>>(null);
+	const listRef = useRef<FlatList>(null);
 	const [scrollOffset, setScrollOffset] = useState<number>(0);
-	const [isRefreshing, setIsRefreshing] = useState(false);
 
 	const [showActDelConfirm, setShowActDelConfirm] = useState(false);
 	const [actDelID, setActDelID] = useState<number | null>(null);
@@ -56,23 +56,33 @@ const ActivityListPage = () => {
 	// 	setShowActDelConfirm(true);
 	// };
 
-	const RenderItem = ({ item }: { item: UserActivityQuery['Page']['activities'][0] }) => {
-		switch (item.__typename) {
+	const RenderItem = ({
+		item,
+	}: {
+		item: NonNullable<NonNullable<UserActivityQuery['Page']>['activities']>[0];
+	}) => {
+		switch (item?.__typename) {
 			case 'ListActivity':
-				return <ListActivityView data={item as ListActivity} isViewerActivity />;
+				return (
+					<ListActivityView
+						data={item as ListActivity}
+						isViewerActivity={item.user?.id === viewerId}
+					/>
+				);
 			case 'MessageActivity':
-				return <MessageActivityView data={item} isViewerActivity />;
+				return (
+					<MessageActivityView
+						data={item}
+						isViewerActivity={item.recipientId === viewerId}
+					/>
+				);
 			case 'TextActivity':
-				return <TextActivityView data={item} isViewerActivity />;
+				return (
+					<TextActivityView data={item} isViewerActivity={item.user?.id === viewerId} />
+				);
 			default:
 				return null;
 		}
-	};
-
-	const onRefresh = async () => {
-		setIsRefreshing(true);
-		await refetch();
-		setIsRefreshing(false);
 	};
 
 	const mergedData = data?.pages?.flatMap((page) => page?.Page?.activities);
@@ -86,21 +96,27 @@ const ActivityListPage = () => {
 
 	return (
 		<View style={{ width: '100%', height: '100%' }}>
-			<FlashList
+			<FlatList
 				ref={listRef}
 				key={1}
 				data={mergedData as any[]}
 				renderItem={RenderItem}
 				keyExtractor={(item, idx) => idx.toString()}
 				numColumns={1}
-				estimatedItemSize={278}
+				// estimatedItemSize={278}
 				refreshControl={
-					<GorakuRefreshControl onRefresh={refetch} refreshing={isRefreshing} />
+					<GorakuRefreshControl onRefresh={refetch} refreshing={isRefetching} />
 				}
 				onEndReached={() => {
 					hasNextPage && fetchNextPage();
 				}}
-				ItemSeparatorComponent={() => <Divider />}
+				ItemSeparatorComponent={() => (
+					<View
+						style={{
+							height: 4,
+						}}
+					/>
+				)}
 				onScroll={(e) => setScrollOffset(e.nativeEvent.contentOffset.y)}
 			/>
 			{scrollOffset > 500 && (
