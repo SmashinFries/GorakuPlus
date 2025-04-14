e8d3196fae6900f1f24788f3771aa0f6c9601e2d
Smashinfries <jus10luka@gmail.com>
2025-04-14 16:57:58 -0500

    fix: handle undefined values in user statistics for anime and manga tabs

--- src/app/user/[username]/stats/[statType].tsx
+++ src/app/user/[username]/stats/[statType].tsx
@@ -1,8 +1,4 @@
-import {
-	MediaType,
-	useUserAnimeStatsQuery,
-	useUserMangaStatsQuery,
-} from '@/api/anilist/__genereated__/gql';
+import { useUserAnimeStatsQuery, useUserMangaStatsQuery } from '@/api/anilist/__genereated__/gql';
 import { GorakuActivityIndicator } from '@/components/loading';
 import { GeneralStatBlock } from '@/components/statistics/blocks';
 import { CountryPie, FormatPie, StatusPie } from '@/components/statistics/pies';
@@ -41,19 +37,20 @@ const AnimeStatTab = ({ username }: StatTabProps) => {
 				<GeneralStatBlock
 					icon="television"
 					title="Total Anime"
-					value={data.User?.statistics?.anime?.count}
+					value={data?.User?.statistics?.anime?.count ?? 0}
 				/>
 				<GeneralStatBlock
 					icon="play"
 					title="Episodes Watched"
-					value={data.User?.statistics?.anime?.episodesWatched}
+					value={data?.User?.statistics?.anime?.episodesWatched ?? 0}
 				/>
 				<GeneralStatBlock
 					icon="calendar-month"
 					title="Days Watched"
 					value={
-						(data.User?.statistics?.anime?.minutesWatched / 60 / 24)?.toFixed(2) ??
-						'???'
+						((data?.User?.statistics?.anime?.minutesWatched ?? 1) / 60 / 24)?.toFixed(
+							2,
+						) ?? '???'
 					}
 				/>
 				<GeneralStatBlock
@@ -61,26 +58,26 @@ const AnimeStatTab = ({ username }: StatTabProps) => {
 					title="Days Planned"
 					value={
 						(
-							data.User?.statistics?.anime?.statuses?.find(
-								(item) => item.status === 'PLANNING',
-							)?.minutesWatched / 1440
+							(data?.User?.statistics?.anime?.statuses?.find(
+								(item) => item?.status === 'PLANNING',
+							)?.minutesWatched ?? 1) / 1440
 						)?.toFixed(2) ?? '???'
 					}
 				/>
 				<GeneralStatBlock
 					icon="division"
 					title="Mean Score"
-					value={data.User?.statistics?.anime?.meanScore ?? '???'}
+					value={data?.User?.statistics?.anime?.meanScore ?? '???'}
 				/>
 				<GeneralStatBlock
 					icon="division"
 					title="Standard Deviation"
-					value={data.User?.statistics?.anime?.standardDeviation ?? '???'}
+					value={data?.User?.statistics?.anime?.standardDeviation ?? '???'}
 				/>
 				{/* <ScoreGraph scores={data.User?.statistics?.anime?.scores} /> */}
-				<FormatPie data={data.User?.statistics?.anime.formats} />
-				<StatusPie data={data.User?.statistics?.anime.statuses} />
-				<CountryPie data={data.User?.statistics?.anime.countries} />
+				<FormatPie data={data?.User?.statistics?.anime?.formats} />
+				<StatusPie data={data?.User?.statistics?.anime?.statuses} />
+				<CountryPie data={data?.User?.statistics?.anime?.countries} />
 				{/* <ReleaseYearsLineChart data={data.User?.statistics?.anime.releaseYears} /> */}
 			</View>
 			<View style={{ flex: 1 }}>
@@ -115,40 +112,40 @@ const MangaStatTab = ({ username }: StatTabProps) => {
 				<GeneralStatBlock
 					icon="television"
 					title="Total Manga"
-					value={data.User?.statistics?.manga?.count}
+					value={data?.User?.statistics?.manga?.count ?? 0}
 				/>
 				<GeneralStatBlock
 					icon="play"
 					title="Chapters Read"
-					value={data.User?.statistics?.manga?.chaptersRead}
+					value={data?.User?.statistics?.manga?.chaptersRead ?? 0}
 				/>
 				<GeneralStatBlock
 					icon="play"
 					title="Volumes Read"
-					value={data.User?.statistics?.manga?.volumesRead}
+					value={data?.User?.statistics?.manga?.volumesRead ?? 0}
 				/>
 				<GeneralStatBlock
 					icon="timer-sand-full"
 					title="Chapters Planned"
 					value={
-						data.User?.statistics?.manga?.statuses?.find(
-							(item) => item.status === 'PLANNING',
+						data?.User?.statistics?.manga?.statuses?.find(
+							(item) => item?.status === 'PLANNING',
 						)?.chaptersRead ?? '???'
 					}
 				/>
 				<GeneralStatBlock
 					icon="division"
 					title="Mean Score"
-					value={data.User?.statistics?.manga?.meanScore ?? '???'}
+					value={data?.User?.statistics?.manga?.meanScore ?? '???'}
 				/>
 				<GeneralStatBlock
 					icon="division"
 					title="Standard Deviation"
-					value={data.User?.statistics?.manga?.standardDeviation ?? '???'}
+					value={data?.User?.statistics?.manga?.standardDeviation ?? '???'}
 				/>
-				<FormatPie data={data.User?.statistics?.manga.formats} />
-				<StatusPie data={data.User?.statistics?.manga.statuses} />
-				<CountryPie data={data.User?.statistics?.manga.countries} />
+				<FormatPie data={data?.User?.statistics?.manga?.formats} />
+				<StatusPie data={data?.User?.statistics?.manga?.statuses} />
+				<CountryPie data={data?.User?.statistics?.manga?.countries} />
 				{/* <ScoreGraph scores={data.User?.statistics?.anime?.scores} /> */}
 			</View>
 			<View style={{ flex: 1 }}>
