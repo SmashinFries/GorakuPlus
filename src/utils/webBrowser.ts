b9e245360e8d1b9ae953e2f7a4080a855bf9e1e3
Smashinfries <jus10luka@gmail.com>
2025-05-05 16:23:47 -0500

    fix: update openWebBrowser to use preferred browser package for consistency

--- src/utils/webBrowser.ts
+++ src/utils/webBrowser.ts
@@ -8,9 +8,6 @@ export const openWebBrowser = async (link?: string | null, openInBrowser?: boole
 		await Linking.openURL(link);
 	} else {
 		const customtabs = await WebBrowser.getCustomTabsSupportingBrowsersAsync();
-		await WebBrowser.openBrowserAsync(link, {
-			// createTask: false,
-			browserPackage: customtabs.servicePackages[0],
-		});
+		await WebBrowser.openBrowserAsync(link, { createTask: true, browserPackage: customtabs.preferredBrowserPackage });
 	}
 };
